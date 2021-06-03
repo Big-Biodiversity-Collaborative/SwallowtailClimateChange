a59c344d6e78476d69abc2b41f3f8b29066902ac
Jeff Oliver <jcoliver@email.arizona.edu>
2021-06-02 18:08:56 -0700

    Minor cleanup in model script creation

--- build-sdm-files.sh
+++ build-sdm-files.sh
@@ -1,4 +1,4 @@
-# Create SDM files for each species
+# Create an SDM script for each species
 # Jeff Oliver
 # jcoliver@arizona.edu
 # 2021-06-02
@@ -7,19 +7,18 @@
 # readarray -t NAMES < <(tail -n +2 data/gbif-reconcile.csv)
 # for testing on two lines
 readarray -t NAMES < <(tail -n +2 data/gbif-reconcile.csv | head -n 2)
-# echo "${NAMES[0]}"
 
 # Iterate over all lines in that gbif names file
-for INDEX in "${!NAMES[@]}"
+for LINE in "${NAMES[@]}"
 do
-    # echo "$INDEX ${NAMES[INDEX]}"
+    # echo "$LINE"
     # Create an array out of that line
-    IFS="," read -r -a ONENAME <<< "${NAMES[INDEX]}"
+    IFS="," read -r -a ONENAME <<< "${LINE}"
     GENUS=$(echo "${ONENAME[0]}" | sed 's/\"//g')
     SPECIES=$(echo "${ONENAME[1]}" | sed 's/\"//g')
     # echo "GENUS: ${GENUS}, SPECIES: ${SPECIES}"
     FILENAME="${GENUS}_${SPECIES}-sdm.R"
     MODELFILE=$(cat template-model-building.R)
     echo "${MODELFILE}" | sed "s/GENUS/${GENUS}/g" | sed "s/SPECIES/${SPECIES}/g" > "$FILENAME"
-    echo "${FILENAME}"
+    echo "Wrote to ${FILENAME}"
 done;
