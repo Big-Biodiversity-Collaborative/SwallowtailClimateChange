acefe4075feac372a166aada24078a4e9c4629cc
Erin Zylstra <erinzylstra@gmail.com>
2022-05-04 12:44:22 -0700

    Count number of gbif records by decade

--- src/data/count-gbif-occurrences.R
+++ src/data/count-gbif-occurrences.R
@@ -1,4 +1,4 @@
-# Estimate data reduction when restricting GBIF data to recent years
+# Estimate amount of GBIF data in each decade
 # Erin Zylstra
 # ezylstra@arizona.edu
 # 2022-04-30
@@ -10,24 +10,27 @@ unzip(zipfile = "data/gbif.zip")
 
 gbif_files <- list.files(path = "data/gbif",
                          pattern = "*-gbif.csv",
-                         full.names=TRUE)
-year_list <- 2000:2022 
+                         full.names = TRUE)
 
-for(one_file in gbif_files){
-  dat <- read.csv(file = one_file)
-  
-  # Count the total number of occurrence records (note that this includes records where year = NA)
-  total <- nrow(dat)
-  
-  # Count the number of occurrences in year_list
-  recent <- sum(dat$year %in% year_list)
+# Create decadal bins for occurrence records (and one pre-1900 bin)
+decades <- c(0, seq(1900, 2030, by=10))
 
-  # If this is the first record, we need to create the dataframe  
-  if (one_file == gbif_files[1]) {
-    spp_nocc <- data.frame(species=dat$species[1],total_occ=total,recent_occ=recent)
-  } else {
-    spp_nocc <- spp_nocc %>% bind_rows(data.frame(species=dat$species[1],total_occ=total,recent_occ=recent))
-  }
+# Create empty dataframe to store number of records in each decade (e.g., d2000 = 2000-2009)
+spp_nocc <- data.frame(matrix(NA, nrow = length(gbif_files), ncol = 16))
+colnames(spp_nocc) <- c("species", 
+                        "pre1900", 
+                        paste0("d",seq(1900, 2020, by = 10)),
+                        "date_missing")
+
+for(i in 1:length(gbif_files)){
+  dat <- read.csv(file = gbif_files[i])
+  spp_nocc[i,1] <- dat$species[1]
+  
+  # Count the number of occurrence records in each decade
+  spp_nocc[i,2:15] <- as.numeric(table(cut(dat$year, breaks = decades, right = FALSE)))
+  
+  # Count the number of records where year = NA
+  spp_nocc[i,16] <- sum(is.na(dat$year))
 }  
 
 write.csv(spp_nocc, 
