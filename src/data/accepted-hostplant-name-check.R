98fb344e2368a6940b6684b0a67b7d4050edd53f
Erin Zylstra <erinzylstra@gmail.com>
2022-05-18 13:11:57 -0700

    Update accepted names of hostplants in csv files

--- src/data/accepted-hostplant-name-check.R
+++ src/data/accepted-hostplant-name-check.R
@@ -1,4 +1,4 @@
-# Add accepted name of hostplants to gbif-reconcile.csv and insect-host.csv
+# Update accepted names of hostplants in gbif-reconcile.csv and insect-host.csv
 # Erin Zylstra
 # ezylstra@arizona.edu
 # 2022-05-16
@@ -6,43 +6,48 @@
 require(TNRS)
 require(dplyr)
 
-# TO DO: 
-# Modify the code above to check or update accepted names in gbif-reconcile.csv
-# (Current version adds an accepted_name column assuming it doesn't already exist)
-# Output updated csvs
-
 # Read in files
 rec <- read.csv(file = "data/gbif-reconcile.csv")
 insect_host <- read.csv(file = "data/insect-host.csv")
 
-# Extract hostplant names from gbif-reconcile
-gbif_hosts <- subset(rec, genus != "Papilio", select = c(genus, species))
-gbif_hosts$Genus_species <- paste(gbif_hosts$genus, gbif_hosts$species, sep = " ")
+# For convenience, temporarily create Genus_species column in rec
+rec$Genus_species <- paste(rec$genus, rec$species, sep = " ")
+
+# Extract (original) hostplant names from gbif-reconcile
+# (note: we may get rid of these columns and just go with accepted name eventually)
+gbif_hosts <- subset(rec, genus != "Papilio", select = c(Genus_species, accepted_name))
 
 # Use TNRS package to find the accepted name for each hostplant
 # (using default taxonomic sources: TROPICOS, WCVP)
-gbif_hosts$accepted_name <- 
-  TNRS(taxonomic_names = gbif_hosts$Genus_species)[,"Accepted_species"]
+gbif_hosts$accepted_new <- TNRS(gbif_hosts$Genus_species)[,"Accepted_species"]
 
-# Which species have different accepted names?
-filter(gbif_hosts, Genus_species != accepted_name)
+# Extract (original names of) hostplants that need to be updated
+spp_update <- 
+  gbif_hosts$Genus_species[which(gbif_hosts$accepted_name != gbif_hosts$accepted_new)]
 
-# Merge accepted names with original csv
-rec <- left_join(rec, select(gbif_hosts, -Genus_species))
+if (length(spp_update) == 0) {
+  message("Accepted names of hostplants already up to date")
+} else {
+  for (species in spp_update) {
+    message(paste0("Updating accepted name for ", species))
+    new_name <- gbif_hosts$accepted_new[gbif_hosts$Genus_species == species]
+    # Update accepted names in gbif-reconcile (rec) and insect-host
+    rec <- rec %>% mutate(accepted_name = replace(accepted_name, 
+                                                  Genus_species == species,
+                                                  new_name))
+    insect_host <- insect_host %>% mutate(host_accepted = replace(host_accepted,
+                                                                  host == species,
+                                                                  new_name))
+  } 
+}
 
-# Add in insect accepted names (assuming genus and species columns are correct)
-rec$accepted_name <- ifelse(is.na(rec$accepted_name),
-                                 paste(rec$genus, rec$species, sep = " "),
-                                 rec$accepted_name)
-rec <- relocate(rec, accepted_name, .after = gbif_name)
-
-# Append hostplant accepted names to insect-host
-gbif_hosts <- data.frame(host = gbif_hosts$Genus_species, 
-                         host_accepted = gbif_hosts$accepted_name)
-insect_host <- left_join(insect_host, gbif_hosts)
-insect_host <- relocate(insect_host, host_accepted, .before = source)
-
-# Which species have different accepted names?
-filter(insect_host, host != host_accepted)
-
+# If any names have been updated, overwrite gbif-reconcile and insect-host csvs
+if (length(spp_update) > 0) {
+  write.csv(select(rec, -Genus_species),
+            file = "data/gbif-reconcile.csv",
+            row.names = FALSE)
+  write.csv(insect_host,
+            file = "data/insect-host.csv",
+            row.names = FALSE)
+}
 
