f13f83b4f58943fe623e6a95778b53d29e0e9e06
Erin Zylstra <erinzylstra@gmail.com>
2022-05-20 15:55:46 -0700

    Create prediction scripts using accepted name

--- src/bash/build-scripts-prediction.sh
+++ src/bash/build-scripts-prediction.sh
@@ -23,8 +23,9 @@ do
     IFS="," read -r -a ONENAME <<< "${LINE}"
   
     # Extract genus & species name
-    GENUS=$(echo "${ONENAME[0]}" | sed 's/\"//g')
-    SPECIES=$(echo "${ONENAME[1]}" | sed 's/\"//g')
+    GENUSSPECIES=$(echo "${ONENAME[3]}" | sed 's/\"//g')
+    GENUS=$(echo "$GENUSSPECIES" | cut -d' ' -f 1)
+    SPECIES=$(echo "$GENUSSPECIES" | cut -d' ' -f 2)
     # echo "GENUS: ${GENUS}, SPECIES: ${SPECIES}"
   
     # For the filename, want genus to be lower case
