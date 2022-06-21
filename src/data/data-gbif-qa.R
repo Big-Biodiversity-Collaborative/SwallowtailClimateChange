2f6f5555d897274669619e20345ca5d81d969594
Erin Zylstra <erinzylstra@gmail.com>
2022-06-21 13:45:47 -0700

    Exclude records for which climate data are unavailable

--- src/data/data-gbif-qa.R
+++ src/data/data-gbif-qa.R
@@ -1,56 +1,73 @@
 # GBIF data QA/QC
-# Jeff Oliver
-# jcoliver@arizona.edu
-# 2021-06-03
+# Jeff Oliver & Erin Zylstra
+# jcoliver@arizona.edu; ezylstra@arizona.edu
+# 2022-06-21
 
+require(terra)
 require(dplyr)
+
 # Need to check each gbif file to make sure 
-#     observation are restricted to CA, MX, US
-#     are geographically bounded to north america (roughly)
+#     observations are restricted to CA, MX, US
+#     in locations where climate data are available
 
 # Logical indicating whether or not to update data files, removing any 
-# observations that are out of bounds (i.e. roughly outside North America 
-# longitude and latitude)
+# observations that are out of bounds
 remove_oob <- TRUE
 
+# Get list of files with gbif data
 gbif_files <- list.files(path = "data/gbif",
                          pattern = "*-gbif.csv",
                          full.names = TRUE)
-lat_lim <- c(14, 80)
-lon_lim <- c(-170, -52)
 
-oob <- NULL
-for (one_file in gbif_files) {
-  data <- read.csv(file = one_file)
-  # Identify any that are out of the rough lat/lon bounds of North America
-  any_oob <- data %>%
-    filter(longitude < lon_lim[1] | longitude > lon_lim[2] |
-             latitude < lat_lim[1] | latitude > lat_lim[2])
+# Load a tif file with climate data
+tif_file <- list.files(path = "data/wc2-1", 
+                       pattern = ".tif$", 
+                       full.names = TRUE)[1]
+clim_data <- rast(tif_file)
+
+# Create a table that will summarize the number of excluded records per spp
+gbif_obs <- as.data.frame(matrix(NA, nrow = length(gbif_files), ncol = 3))
+colnames(gbif_obs) <- c("species", "n_orig", "n_filtered")
+
+for (i in 1:length(gbif_files)) {
   
-  if (nrow(any_oob) > 0) {
-    if (is.null(oob)) {
-      oob <- any_oob
-    } else {
-      oob <- oob %>%
-        bind_rows(any_oob)
-    }
+  data <- read.csv(file = gbif_files[i])
+  gbif_obs$species[i] <- data$accepted_name[1]
+  gbif_obs$n_orig[i] <- nrow(data)
+  
+  # Extract climate data associated with each gbif record location
+  data$climate <- extract(clim_data, data[,c('longitude','latitude')])[,2]
+  
+  # Identify any records that do not have corresponding climate data
+  any_oob <- data %>%
+    filter(is.na(climate))
+  
+  gbif_obs$n_filtered[i] <- nrow(data) - nrow(any_oob)
+  
+  if (nrow(any_oob) > 0 & remove_oob == TRUE) {
+
+    # If appropriate, update files, removing records that are out of bounds
+    data <- data %>%
+      filter(!(gbifID %in% any_oob$gbifID))
     
-    # If appropriate update data files, removing those beast that are out of 
-    # bounds
-    if (remove_oob) {
-      data <- data %>%
-        filter(!(gbifID %in% any_oob$gbifID))
-      write.csv(x = data,
-                file = one_file,
-                row.names = FALSE)
-    }
+    # write.csv(x = data,
+    #           file = gbif_files[i],
+    #           row.names = FALSE)
   }
 }
 
+# How many records were excluded?
+gbif_obs$n_excluded <- gbif_obs$n_orig - gbif_obs$n_filtered
+gbif_obs$perc_excluded <- round(gbif_obs$n_excluded / gbif_obs$n_orig * 100, 2)
+arrange(gbif_obs, desc(perc_excluded))
+  # Papilio brevicauda: 7.23% (46 records) excluded
+  # All other species with < 5% of records excluded 
+  # 93% of species with < 1% of records excluded
+
 # Create a zip file of all the gbif flies (first removing previous archive)
-zipfile <- "data/gbif.zip"
-if (file.exists(zipfile)) {
-  invisible(file.remove(zipfile))
-}
-zip(zipfile = "data/gbif.zip",
-    files = gbif_files)
+# zipfile <- "data/gbif.zip"
+# if (file.exists(zipfile)) {
+#   invisible(file.remove(zipfile))
+# }
+# zip(zipfile = "data/gbif.zip",
+#     files = gbif_files)
