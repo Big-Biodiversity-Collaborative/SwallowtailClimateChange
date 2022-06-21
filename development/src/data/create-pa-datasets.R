955ff38a9711f87dbceed2570b3e3fccbf1fe206
Erin Zylstra <erinzylstra@gmail.com>
2022-06-21 15:42:20 -0700

    Filter by year and convert to terra

--- development/src/data/create-pa-datasets.R
+++ development/src/data/create-pa-datasets.R
@@ -3,27 +3,19 @@
 # ezylstra@arizona.edu
 # 2022-06-13
 
-require(raster)
+require(terra)
+# require(raster)
 require(dplyr)  # load *after* raster for easier use of select
-require(dismo)  # background point sampling
+# require(dismo)  # background point sampling
 
 # Load up the functions from the functions folder
 source(file = "load_functions.R")
 
-replace <- FALSE
+replace <- TRUE
 verbose <- TRUE
 
 # Read in gbif-reconcile
 species_list <- read.csv("development/data/gbif-reconcile-4spp.csv")
-
-# Using the first bil file to create a raster to use as mask for sampling 
-# background points
-bil_file <- list.files(path = "data/wc2-1", 
-                       pattern = ".tif$", 
-                       full.names = TRUE)[1]
-mask <- raster(bil_file)
-# Do not need this anymore
-rm(bil_file)
 
 for (species in species_list$accepted_name) {
   nice_name <- tolower(x = gsub(pattern = " ",
@@ -38,7 +30,7 @@ for (species in species_list$accepted_name) {
     }    
   }
   
-  # Load in observation data
+  # Load observation data
   obs_file <- paste0("data/gbif/",
                      nice_name,
                      "-gbif.csv")
@@ -47,50 +39,54 @@ for (species in species_list$accepted_name) {
   }
   obs <- read.csv(file = obs_file)
   
-  # Get the geographic extent of the observation data
-  obs_extent <- get_extent(data = obs)  
-  
-  # Use random sampling to generate pseudo-absence points
-  # mask:  Provides resolution of sampling points
-  # n:     Number of random points
-  # ext:   Spatially restricts sampling
-  # extf:  Expands sampling a little bit
-  absence <- dismo::randomPoints(mask = mask,  
-                                 n = 5000, # Will want to make this bigger
-                                 ext = obs_extent, 
-                                 extf = 1.25)
-  
-  # Convert to data frame
-  absence <- as.data.frame(absence) 
-
-  # Grab worldclim data to use as predictors
-  predictors <- raster::stack(list.files(path = "data/wc2-1",
-                                         pattern = ".tif$",
-                                         full.names = TRUE))
-  
-  # Only need geo coordinates, so extract those (in x, y order)
+  # Restrict observations to the most recent 23-year period (2000-2022) and
+  # retain just the geographic coordinates
   presence <- obs %>%
+    dplyr::filter(year %in% 2000:2022) %>%
     dplyr::select(longitude, latitude)
   
-  # Extract predictor values for observed and background points
-  predictors_presence <- raster::extract(x = predictors, y = presence)
-  predictors_absence <- raster::extract(x = predictors, y = absence)
+  # Get the geographic extent of the observation data
+  obs_extent <- get_extent(data = presence)  
   
+  # Grab worldclim data to use as predictors
+  predictors <- terra::rast(list.files(path = "data/wc2-1",
+                                       pattern = ".tif$",
+                                       full.names = TRUE))  
+
+  # Extract predictor values for observed points
+  presence <- terra::extract(x = predictors, 
+                             y = presence, 
+                             xy = TRUE)
+  
+  # Rearrange columns
+  presence <- presence %>%
+    dplyr::select(-ID) %>%
+    dplyr::relocate(c(x,y), .before = bio1)
+  
+  # Use random sampling to generate pseudo-absence points and extract predictor 
+  # values
+  absence <- terra::spatSample(x = predictors,  
+                               size = 5000, # Will want to make this bigger
+                               method = "random",
+                               na.rm = TRUE,
+                               values = TRUE,
+                               xy = TRUE,
+                               ext = obs_extent * 1.25)
+
   # Make a vector of appropriate length with 0/1 values for 
   # (pseudo)absence/presence
   pa_data <- c(rep(x = 1, times = nrow(presence)), 
-               rep(x = 0, times = nrow(absence)))    
-  
+               rep(x = 0, times = nrow(absence)))  
+
   # Create a vector of folds for easier splitting into testing/training
   num_folds <- 5 # for 20/80 split
   fold <- c(rep(x = 1:num_folds, length.out = nrow(presence)),
             rep(x = 1:num_folds, length.out = nrow(absence)))
   
-  # Combine our presence / absence and fold vectors with environmental data we 
-  # extracted
+  # Combine our presence / absence and fold vectors with environmental data
   full_data <- data.frame(cbind(pa = pa_data,
                                 fold = fold,
-                                rbind(predictors_presence, predictors_absence)))
+                                rbind(presence, absence)))
   write.csv(x = full_data,
             file = filename,
             row.names = FALSE)
