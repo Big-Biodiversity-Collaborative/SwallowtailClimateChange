847ca5f6d6ee42ba4b73654747998a5a0582efc7
Erin Zylstra <erinzylstra@gmail.com>
2022-06-24 16:17:51 -0700

    Run weighted SVMs for PABR and PREM

---
+++ development/src/indiv/prunus_emarginata-model-svmw-kernlab.R
@@ -0,0 +1,42 @@
+# Test run of weighted SVM model using the kernlab package
+# Erin Zylstra
+# ezylstra@arizona.edu
+# 2022-06-14
+
+require(dplyr)
+
+# Load functions from the functions folder
+source(file = "load_functions.R")
+source("development/functions/run_svmw_kernlab.R")
+
+genus <- "Prunus"
+species <- "emarginata"
+
+set.seed(20220614)
+
+# Name for reporting and looking up info in files
+species_name <- paste0(genus, " ", species)
+# A more compute-friendly name
+nice_name <- tolower(paste0(genus, "_", species))
+
+# Load in presence-absence dataset
+pa_file <- paste0("development/data/presence-absence/",
+                   nice_name,
+                   "-pa.csv")
+# if (!file.exists(pa_file)) {
+#   unzip(zipfile = "development/data/pa-datasets.zip")
+# }
+full_data <- read.csv(file = pa_file)
+
+# Run support vector machine model
+svmw_model <- run_svmw_kernlab(full_data = full_data,
+                               verbose = FALSE)
+
+# Save the model to file in output/models/
+model_file <- paste0("development/output/SDMs/", nice_name,
+                     "-sdm-svmw-current.rds")
+saveRDS(object = svmw_model,
+        file = model_file)
+
+message(paste0("Weighted SVM model for ", species_name, 
+               " complete; saved to ", model_file))
