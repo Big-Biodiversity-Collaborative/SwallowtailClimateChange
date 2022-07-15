f679e0ee9da688cdeb256f181247813e948af481
Erin Zylstra <erinzylstra@gmail.com>
2022-07-15 14:15:11 -0700

    Change geographic extent of prediction area

--- development/functions/predict_sdm.R
+++ development/functions/predict_sdm.R
@@ -5,13 +5,13 @@
 #' "papilio_multicaudata" for Papilio multicaudata)
 #' @param predictors bioclimatic data to use for predictions
 #' @param model saved SDM model 
-#' @param pred_extf numeric indicating how far beyond current observations to 
-#' make predictions. e.g. a value of 1.1 will extend the geographic extent by 
-#' 5% on all four sides of the extent of current observations
-predict_sdm <- function(nice_name, 
-                        predictors, 
-                        model,
-                        pred_extf = 1.5) {
+#' @param yr character indicating year for which predictions are being made 
+#' (eg, current, 2041, 2071)
+
+predict_sdm_insect <- function(nice_name, 
+                               predictors, 
+                               model,
+                               yr) {
   if (!require(raster)) {
     stop("predict_sdm requires raster package, but it could not be loaded")
   }
@@ -21,8 +21,11 @@ predict_sdm <- function(nice_name,
   if (!require(dismo)) {
     stop("predict_sdm requires dismo package, but it could not be loaded")
   }
+  if (!require(fields)) {
+    stop("predict_sdm requires fields package, but it could not be loaded")
+  }
   source(file = "functions/get_extent.R")
-
+  
   # Get the current observations, to dictate the geographic extent of the 
   # predicted presence / absence
   pa_file <- paste0("development/data/presence-absence/",
@@ -42,7 +45,32 @@ predict_sdm <- function(nice_name,
     p_extent <- get_extent(data = pa)
     rm(pa)
   }
+  
+  # Want the prediction extent to extend a reasonable amount beyond GBIF obs
+  # Calculate extent based on P. cresphontes movements (Wilson et al. 2021)
+  # Can move 180 km per decade, so ~ 360 km by 2041 or 900 km by 2071
+  # 360 km latitude = 3.24 deg; 900 km latitude = 8.09 deg 
+  # km longitude depends on latitude (lower lat = more km, fewer deg)
+  # Use the fields::rdist.earth to convert 1-degree distance longitude into km
+  # at minimum latitude 
 
+  lon1 <- 100
+  lon2 <- 101
+  lat <- p_extent@ymin
+  km_minlat <- rdist.earth(matrix(c(lon1, lat), ncol = 2),
+                           matrix(c(lon2, lat), ncol = 2),
+                           miles = FALSE,
+                           R = 6371)
+  
+  if (yr %in% c("current", "2041")) {
+    lat_toadd <- 3.24
+    lon_toadd <- 360 * 1/km_minlat
+  } else {
+    lat_toadd <- 8.09
+    lon_toadd <- 900 * 1/km_minlat
+  }
+  pred_extent <- p_extent + c(-lon_toadd, lon_toadd, -lat_toadd, lat_toadd)
+  
   # Calculate probabilities based on predictors and model
   if (is.null(p_extent)) {
     # For some reason, obs not found, so cannot restrict extent
@@ -51,11 +79,11 @@ predict_sdm <- function(nice_name,
   } else {
     preds <- predict(predictors, 
                      model, 
-                     ext = p_extent * pred_extf)
+                     ext = pred_extent)
   }
-    # Note: predict() above is using the raster package
+  # Note: predict() above is using the raster package
   
   # Send back this raster with the predicted values
   return(preds)
-
+  
 }
