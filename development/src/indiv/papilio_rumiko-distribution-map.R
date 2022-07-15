f4673caa853f24da1b7ebb2e1f36eb3f43f3a8e8
Erin Zylstra <erinzylstra@gmail.com>
2022-07-15 15:22:49 -0700

    Create SDM and prediction scripts for PARU

---
+++ development/src/indiv/papilio_rumiko-distribution-map.R
@@ -0,0 +1,28 @@
+# Create a distribution map for Papilio rumiko
+# Erin Zylstra
+# ezylstra@arizona.edu
+# 2022-07-07
+
+# Load up the necessary functions
+source(file = "development/functions/single_map_devel.R")
+
+genus <- "Papilio"
+species <- "rumiko"
+
+model <- "maxent"
+
+time_periods <- c("current", 
+                  "ssp245-2041", "ssp245-2071", 
+                  "ssp370-2041", "ssp370-2071")
+
+# Name for reporting
+species_name <- paste0(genus, " ", species)
+
+distribution_map <- single_map_devel(species_name,
+                                     time_period = time_periods[4], 
+                                     model,
+                                     show_obs = TRUE)
+print(distribution_map)
+
+
+
