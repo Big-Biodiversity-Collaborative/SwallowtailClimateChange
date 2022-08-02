932a81e220f8cb701a96840da34725064ea5475e
Erin Zylstra <erinzylstra@gmail.com>
2022-08-02 11:28:44 -0700

    Remove code used to extract climate data

--- development/src/data/create-pa-datasets_bufferedMCP.R
+++ development/src/data/create-pa-datasets_bufferedMCP.R
@@ -3,9 +3,6 @@
 # ezylstra@arizona.edu
 # 2022-06-13
 
-# Testing the buffered MCP approach for P. rumiko or some other species 
-# (instead of looping through all species in the gbif-reconcile file)
-
 require(terra)
 require(sp)
 require(sf)
@@ -17,20 +14,19 @@ verbose <- TRUE
 # Read in gbif-reconcile
 species_list <- read.csv("development/data/gbif-reconcile-13spp.csv")
 
-#for (species in species_list$accepted_name) {
+for (species in species_list$accepted_name) {
 
-  species <- "Ruta graveolens"
   nice_name <- tolower(x = gsub(pattern = " ",
                                 replacement = "_",
                                 x = species))
   filename <- paste0("development/data/presence-absence/", nice_name, "-pa.csv") 
 
   # Only proceed if file doesn't exist or we want to replace existing files
-  # if (file.exists(filename) & replace == FALSE) next
-  #   
-  #   if (verbose) {
-  #     message(paste0("\n****  Beginning process for ", species, "  ****"))
-  #   }    
+  if (file.exists(filename) & replace == FALSE) next
+
+    if (verbose) {
+      message(paste0("\n****  Beginning process for ", species, "  ****"))
+    }
 
   # Load observation data
   obs_file <- paste0("data/gbif/",
@@ -40,47 +36,28 @@ species_list <- read.csv("development/data/gbif-reconcile-13spp.csv")
     unzip(zipfile = "data/gbif.zip")
   }
   obs <- read.csv(file = obs_file)
-  
-  # Outside the development folder, we'll be using observations that have
-  # already been filtered by date and location (where climate data are avail)
-  # and a ~95% density envelope. To avoid including outliers when testing this 
-  # MCP approach, I'll manually restrict observations to certain lat/longs
-    # For testing P. rumiko, filter out observation with latitude >= 35 deg
-    # For testing P. cresphontes, filter lat >= 48 deg, long > -121
-    # For testing A. texana, filter >32, long outside -100.5 to -96.5
-    # For testing C. greggii, filter lat < 20
-    # For testing P. trifoliata, filter lat > 48, long < -115
-    # For testing R. graveolens, filter long < -130
-    # For testing Z. americanum, filter long < -100
-    # For testing Z. clava-herculis, filter long < -104, lat >39
-    # For testing Z. fagara, filter lat > 35, long < -115
-  
-    plot(latitude~longitude,obs,pch=19,cex=0.6)
-    abline(h=35,col='blue')
-    abline(v=-130, col = "blue")
-
-  obs <- obs %>%
-    dplyr::filter(longitude > (-130)) %>%
-    dplyr::filter(latitude < 35)
 
   # Restrict observations to the most recent 23-year period (2000-2022) and
   # retain just the geographic coordinates
   presence <- obs %>%
     dplyr::filter(year %in% 2000:2022) %>%
-    dplyr::select(longitude, latitude)
+    dplyr::select(longitude, latitude) %>%
+    dplyr::rename(x = longitude,
+                  y =latitude)
   
-  # TODO: remove duplicate locations first? (JUST FOR BUFFER CALCS)
+  # Remove duplicate locations just for calculating nearest neighbor distances
+  presence1 <- distinct(presence)
 
   # Convert to a SpatialPoints object
-  obs_spatial <- SpatialPoints(coords = presence,
-                               proj4string = CRS("+init=epsg:4326"))
+  presence1_sp <- SpatialPoints(coords = presence1,
+                                proj4string = CRS("+init=epsg:4326"))
   
   # Calculate the GreatCircle distance (in km) between points
-  gc_dist <- sp::spDists(obs_spatial, longlat = TRUE) 
+  gc_dist <- sp::spDists(presence1_sp, longlat = TRUE) 
 
   # Calculate the maximum of nearest neighbor distances (in km)
   buffer <- gc_dist %>%
-    apply(., 1, function(x) min(x[x>0])) %>%
+    apply(., 1, function(x) min(x[x > 0])) %>%
     max %>%
     round
   rm(gc_dist)
@@ -94,17 +71,17 @@ species_list <- read.csv("development/data/gbif-reconcile-13spp.csv")
   # Convert MCP to sf object and project to NA Albers Equal Area Conic 
   ch_poly_proj <- st_as_sf(ch_polygon) %>%
     st_transform(crs = "ESRI:102008")
-    # Check projection:
-    # st_crs(ch_poly_proj)
-  
+
   # Create a polygon = MCP + buffer
   ch_buffer <- st_buffer(ch_poly_proj,
                          dist = buffer * 1000)
 
-  # Grab worldclim data to use as predictors
-  predictors <- terra::rast(list.files(path = "data/wc2-1",
-                                       pattern = ".tif$",
-                                       full.names = TRUE))  
+  # Grab one of the climate tif files to use as a mask to generate 
+  # pseudo-absence points
+  tif_file <- list.files(path = "data/wc2-1", 
+                         pattern = ".tif$", 
+                         full.names = TRUE)[1]
+  predictor <- terra::rast(tif_file)
   
   # Transform the buffered MCP back to lat/long 
   ch_buffer_latlong <- st_transform(ch_buffer, 4326)
@@ -112,28 +89,18 @@ species_list <- read.csv("development/data/gbif-reconcile-13spp.csv")
   # Convert the buffered MCP to a SpatVector
   ch_buffer_sv <- terra::vect(ch_buffer_latlong)
   
-  # Crop and mask predictors to the buffered MCP polygon
-  pred_mask <- predictors %>%
+  # Crop and mask climate data to the buffered MCP polygon
+  pred_mask <- predictor %>%
     terra::crop(ch_buffer_sv) %>%
     terra::mask(ch_buffer_sv)
-  rm(predictors)
-  
-  # Extract predictor values for observed points
-  presence <- terra::extract(x = pred_mask, 
-                             y = presence, 
-                             xy = TRUE)
-  
-  # Rearrange columns
-  presence <- presence %>%
-    dplyr::select(-ID) %>%
-    dplyr::relocate(c(x,y), .before = bio1)  
-
+  rm(predictor)
+ 
   # Generate pseudo-absence points and extract predictor values
   absence <- terra::spatSample(x = pred_mask,  
                                size = 5000,
                                method = "random",
                                na.rm = TRUE,
-                               values = TRUE,
+                               values = FALSE,
                                xy = TRUE)
   # Note: if buffered area is small, function may not be able to generate the 
   # indicated number of points (size) because the default is to select cells
@@ -165,10 +132,10 @@ species_list <- read.csv("development/data/gbif-reconcile-13spp.csv")
   #           row.names = FALSE)
   # 
   # if (verbose) {
-  #   message(paste0("\n****  ",nrow(presence), " gbif records and ", 
+  #   message(paste0("\n****  ",nrow(presence), " gbif records and ",
   #                  nrow(absence), " pseudo-absence records written to ",
   #                  filename, "  ****"))
   # }
-#}
+}
 
 # Will likely want to put all the csv files into data/pa-datasets.zip
