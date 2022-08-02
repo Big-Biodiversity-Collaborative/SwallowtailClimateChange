681a0ed67c008e1aceb7a0b916145779870f8221
Erin Zylstra <erinzylstra@gmail.com>
2022-08-02 15:44:30 -0700

    Add code to extract climate values for pa data

--- development/src/indiv/papilio_rumiko-sdm-maxent-notune.R
+++ development/src/indiv/papilio_rumiko-sdm-maxent-notune.R
@@ -3,6 +3,7 @@
 # ezylstra@arizona.edu
 # 2022-07-11
 
+require(raster)
 require(dplyr)
 
 # Load functions from the functions folder
@@ -26,11 +27,26 @@ pa_file <- paste0("development/data/presence-absence/",
 # if (!file.exists(pa_file)) {
 #   unzip(zipfile = "development/data/pa-datasets.zip")
 # }
-full_data <- read.csv(file = pa_file)
+pa_data <- read.csv(file = pa_file)
+
+# Only need geo coordinates, so extract those (in x, y order)
+pa_locs <- pa_data %>%
+  dplyr::select(x, y)
+
+# Grab worldclim data to use as predictors
+predictors <- raster::stack(list.files(path = "data/wc2-1",
+                                       pattern = ".tif$",
+                                       full.names = TRUE))
+
+# Extract climate values
+pa_climate <- raster::extract(x = predictors, y = pa_locs) 
+
+# Combine pa and fold data with climate values
+full_data <- cbind(pa_data, pa_climate)
 
 # Run support vector machine model
 maxent_model <- run_maxent_notune(full_data = full_data,
-                                  verbose = FALSE)
+                                  verbose = TRUE)
 
 # Save the model to file in output/models/
 model_file <- paste0("development/output/SDMs/", nice_name,
