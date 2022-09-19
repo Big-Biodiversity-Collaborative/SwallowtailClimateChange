41357da162fe63a29a498da3cc45aac45485b7ec
Rachel Laura <73559122+rachel-laura@users.noreply.github.com>
2022-09-19 14:31:31 -0700

    start work on gam model

---
+++ functions/run_gam.R
@@ -0,0 +1,109 @@
+#' Run generalized linear model species distribution model
+#' 
+#' @param full_data dataframe with presence-absence data (1/0), fold (for 
+#' separating testing and training data), and 19 columns with climate data
+#' @param verbose logical indicating whether or not to print processing messages
+#' 
+#' @details Uses \code{stats::glm()} for generalized linear model
+#' 
+#' @return a list with the following elements:
+#' \describe{
+#'   \item{model}{Generalized linear model SDM; the output of \code{stats::glm}}
+#'   with family = "logit"
+#'   \item{evaluation}{Evaluation of model using testing data; the output of 
+#'   \code{dismo::evaluate}}
+#'   \item{thresh}{Threshold value of probabilities for determining absence or 
+#'   presence; the output of \code{dismo::threshold} with \code{stat = "spec_sens"}}
+#' }
+run_glm <- function(full_data, verbose = TRUE) {
+  # Extract the name of this function for reporting
+  function_name <- as.character(match.call())[1]
+  
+  # Libraries required for this function to work
+  method_name <- "generalized linear model"
+  dependencies <- c("dplyr", "dismo")
+  if (!all(unlist(lapply(X = dependencies, FUN = require, character.only = TRUE)))) {
+    stop("At least one package required by ", function_name, 
+         " could not be loaded: ", paste(dependencies, collapse = ", "),
+         " are required.")
+  }
+  
+  # 
+  # if (!require(dplyr)) {
+  #   stop("run_glm requires dplyr package, but it could not be loaded")
+  # }
+  # if (!require(dismo)) {
+  #   stop("run_glm requires dismo package, but it could not be loaded")
+  # }
+
+  # Make sure presence-absence data are there
+  if (!("pa" %in% colnames(full_data))) {
+    stop(function_name, " requires column named 'pa' in full_data")
+  }
+  # Make sure fold indicators are there
+  if (!("fold" %in% colnames(full_data))) {
+    stop(function_name, " requires column named 'fold' in full_data")
+  }
+  # Make sure data for all 19 climate variables are there
+  if (length(setdiff(paste0("bio",1:19),colnames(full_data))) > 0) {
+    stop(function_name, " requires bio1:bio19 columns in full_data")
+  }
+
+  predvars <- paste0("bio", 1:19)
+  # Create separate data frames for testing and training presence data
+  presence_train <- full_data %>%
+    filter(pa == 1) %>%
+    filter(fold != 1)
+  presence_test <- full_data %>%
+    filter(pa == 1) %>%
+    filter(fold == 1) %>%
+    dplyr::select(all_of(predvars))
+  # Create separate data frames for testing and training (pseudo)absence data
+  absence_train <- full_data %>%
+    filter(pa == 0) %>%
+    filter(fold != 1)
+  absence_test <- full_data %>%
+    filter(pa == 0) %>%
+    filter(fold == 1) %>%
+    dplyr::select(all_of(predvars))
+  
+  # Add presence and pseudoabsence training data into single data frame
+  sdmtrain <- rbind(presence_train, absence_train)
+  
+  if(verbose) {
+    message("Running ", method_name, ".")
+  }  
+  # Run the model, specifying model with standard formula syntax
+  # Exclude bio3 (a function of bio2 & bio7) and bio7 (a function of bio5 and 
+  # bio6)
+  model_fit <- stats::glm(pa ~ bio1 + bio2 + bio4 + bio5 + bio6 +
+                            bio8 + bio9 + bio10 + bio11 + bio12 +
+                            bio13 + bio14 + bio15 + bio16 + bio17 + bio18 +
+                            bio19,
+                          data = sdmtrain,
+                          family = binomial(link = "logit"))
+  
+  if(verbose) {
+    message("Model complete. Evaluating ", method_name, 
+            " with testing data.")
+  }
+  # Evaluate model performance with testing data
+  # The type = "response" is passed to predict.glm so the values are on the 
+  # scale of 0 to 1 (probabilities), rather than the log odds. Required to make
+  # sure the threshold is on the same scale of output from predict_sdm
+  model_eval <- dismo::evaluate(p = presence_test, 
+                                a = absence_test, 
+                                model = model_fit,
+                                type = "response") 
+  
+  # Calculate threshold so we can make a P/A map later
+  pres_threshold <- dismo::threshold(x = model_eval, 
+                                     stat = "spec_sens")
+  
+  # Bind everything together and return as list  
+  results <- list(model = model_fit,
+                  evaluation = model_eval,
+                  thresh = pres_threshold)
+  
+  return(results)
+}
