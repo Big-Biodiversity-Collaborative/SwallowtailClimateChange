520a3e71826543d88257ac8bde17c162c266d8a5
Erin Zylstra <erinzylstra@gmail.com>
2022-12-28 10:34:50 -0700

    Create message to identify missing host plants

--- src/summary/create-overlap-rasters.R
+++ src/summary/create-overlap-rasters.R
@@ -15,6 +15,7 @@ require(stringr)
 # 3 = Insect and at least one host plant predicted present
 
 # TODO: save overlap maps (eg, pngs) to file?
+# TODO: Output messages/warnings to a log file for record keeping?
 
 # Load insect-host file
 ih <- read.csv("data/insect-host.csv")
@@ -41,19 +42,19 @@ if (all_insects) {
 # Loop through insect species to create an overlap raster for each climate model
 for (i in 1:length(insects)) {
   
-  # Identify host plants
+  # Find consensus rasters for the insect
   insect <- insects[i]
   insect_nice_name <- insect %>%
     str_replace(pattern = " ", replacement = "_") %>%
     tolower()
   
   insect_files <- list.files("output/consensus-rasters/", 
-                             pattern = paste(insect_nice_name, collapse = "|"),
+                             pattern = insect_nice_name,
                              full.names = TRUE)
   
-  # Abort if there's no consensus raster for the insect
-  if (length(insect_files) == 0) {
-    message("*** No consensus rasters for ", insect, 
+  # Abort if missing one or more consensus rasters for the insect
+  if (length(insect_files) < 5) {
+    message("*** Missing one or more consensus rasters for ", insect, 
             ". Did not create overlap rasters.")
   } else {
     
@@ -64,10 +65,21 @@ for (i in 1:length(insects)) {
     plant_nice_names <- plants %>%
       str_replace(pattern = " ", replacement = "_") %>%
       tolower()
-    plant_files <- list.files("output/consensus-rasters/", 
-                               pattern = paste(plant_nice_names, collapse = "|"),
-                               full.names = TRUE)    
     
+    plant_files <- NULL
+    for (j in 1:length(plants)) {
+      plant_files_new <- list.files("output/consensus-rasters/", 
+                                    pattern = plant_nice_names[j],
+                                    full.names = TRUE) 
+      # Print message if consensus rasters are missing for a host plant
+      if (length(plant_files_new) == 0) {
+        cat(paste0("Missing consensus rasters for ", 
+                   plants[j], " (host plant for ", insect, ").\n"))
+      } else {
+        plant_files <- c(plant_files, plant_files_new)
+      }
+    }
+
     for (clim_model in climate_models$name) {
       
       dist_files <- str_subset(c(insect_files, plant_files), clim_model)
@@ -75,34 +87,26 @@ for (i in 1:length(insects)) {
       # Abort if consensus rasters missing for all host plants
       if (length(dist_files) == 1) {
         message("*** Missing consensus rasters for all hosts associated with ",
-                insect, ", ", clim_model, 
-                " climate scenario. Did not create overlap raster.")
+                insect, ", ", clim_model, ". Did not create overlap raster.")
       } else {
-        
-        # Print message (but continue with process) if missing a consensus 
-        # raster for one or more (but not all) host plants
-        if (length(dist_files) < length(c(insect, plants))) {
-          cat(paste0("Missing consensus raster for at least 1 host associated with ",
-                     insect, ", ", clim_model, ".\n"))
-        } 
-        
-        # Load consensus raster for insect and convert to binary values (1/0) 
-        # based on the threshold number of SDMs that predict presence (specified
-        # at top of script)
+
+        # Load consensus raster for insect and create a raster with the species'
+        # predicted distribution (ie, convert consensus raster to binary values 
+        # based on the number of SDMs that predict presence)
         insect_dist <- readRDS(dist_files[1])
         insect_dist <- as.numeric(insect_dist >= min_sdms)
 
         # Load consensus rasters for host plants, crop rasters to match extent 
         # of the insect, and convert to binary values. 
         plant_list <- list()
-        for (j in 2:length(dist_files)) {
-          plant_list[[j-1]] <- readRDS(dist_files[j])
-          plant_list[[j-1]] <- as.numeric(plant_list[[j-1]] >= min_sdms)
-          plant_list[[j-1]] <- terra::crop(plant_list[[j-1]], insect_dist)
+        for (j in 1:(length(dist_files) - 1)) {
+          plant_list[[j]] <- readRDS(dist_files[j + 1])
+          plant_list[[j]] <- as.numeric(plant_list[[j]] >= min_sdms)
+          plant_list[[j]] <- terra::crop(plant_list[[j]], insect_dist)
         }
         
         # Calculate spatial extent across all plant rasters
-        all_plants_ext <- ext(sprc(plant_list))
+        all_plants_ext <- ext(terra::sprc(plant_list))
         
         # Extend plant rasters where needed so they can be combined into a
         # single SpatRaster
@@ -110,21 +114,26 @@ for (i in 1:length(insects)) {
           plant_list[[j]] <- terra::extend(plant_list[[j]], all_plants_ext)
         }
         host_dist <- terra::rast(plant_list)
-        
+
         # Calculate the number of host plants present in each cell
         host_dist <- sum(host_dist, na.rm = TRUE)
         
         # Make cell values = 2 where at least one host plant is present, zero 
         # otherwise
-        host_dist <- terra::classify(host_dist, 
-                                     cbind(c(0, 1), c(0, 5), c(0, 2)), 
+          # First create 3-column matrix, with values in columns 1-2 
+          # representing a range of cell values you want to reclassify, and a 
+          # value in the 3rd column containing the new value for those cells.
+          rcl <- rbind(c(0, 0, 0), c(1, 100, 2))
+        host_dist <- terra::classify(x = host_dist, 
+                                     rcl = rcl,
                                      right = NA)
         
         # Combine insect and host distributions 
         
-        # Plant distributions have already been cropped by the insect 
+        # Individual plant distributions have already been cropped by the insect 
         # distribution, but it's possible that the resulting host distribution 
-        # is smaller than the insect distribution.  If so, need to extend it.
+        # is smaller than the insect distribution.  If so, we'll need to extend 
+        # it.
         host_dist <- terra::extend(host_dist, insect_dist)
         
         # Mask host distribution so it has NA values wherever the insect
@@ -147,6 +156,4 @@ for (i in 1:length(insects)) {
       }
     } 
   }
-}      
-
-
+} 
