3973ac3a83a0892109a6b82858ce30973d35ab3b
Erin Zylstra <erinzylstra@gmail.com>
2022-12-29 13:48:17 -0700

    Fix quotes around "rf"

--- src/bash/build-scripts-SDM.sh
+++ src/bash/build-scripts-SDM.sh
@@ -8,7 +8,7 @@
 readarray -t NAMES < <(tail -n +2 data/gbif-reconcile.csv)
 
 SCRIPTTYPE="SDM"
-MODELS=("brt" "gam" "glm" "lasso" "maxent-notune" "maxent-tune" “rf”)
+MODELS=("brt" "gam" "glm" "lasso" "maxent-notune" "maxent-tune" "rf")
 PRODUCT="SDM"
 
 # Iterate over all the types of models (e.g. glm, svm)
