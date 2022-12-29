21dcb8d62a334da04ac42e606dee9125ff1c9b4c
Erin Zylstra <erinzylstra@gmail.com>
2022-12-29 13:53:25 -0700

    Create SpatRasters and not RasterLayers

--- functions/predict_sdm.R
+++ functions/predict_sdm.R
@@ -6,18 +6,18 @@
 #' @param model species distribution model object; generally output from model
 #' function such as maxent or glm
 #' @param sdm_method character indicating the type of SDM ("brt", "glm", "gam", 
-#' "maxent-notune", "maxent-tune", "rf", "svm")
+#' "lasso", "maxent-notune", "maxent-tune", "rf", "svm")
 #' @param yr character indicating year for which predictions are being made 
 #' ("current", "2041", "2071")
 #' @param ssp character indicating shared socioeconomic pathway of global 
 #' climate model; ignored if \code{yr == "current"}
 #' @param stand_obj an object of class save_means_sds (created with the 
 #' save_means_sds function) that contains means and SDs for predictor variables 
-#' in a training dataset (Optional, except for lasso regression models)
+#' in a training dataset (Optional, except for lasso regression and gam models)
 #' @param quad a logical indicating whether or not quadratics were included 
 #' in model (Optional, except for lasso regression and gam models)
 #' 
-#' @return raster of predicted probabilities of occurrence based on given 
+#' @return SpatRaster with predicted probabilities of occurrence based on given 
 #' species distribution model and global climate model data
 
 predict_sdm <- function(nice_name, 
@@ -34,14 +34,11 @@ predict_sdm <- function(nice_name,
   yr <- match.arg(arg = yr)
   ssp <- match.arg(arg = ssp)
   
-  if (!require(raster)) {
-    stop("predict_sdm requires raster package, but it could not be loaded")
+  if (!require(terra)) {
+    stop("predict_sdm requires terra package, but it could not be loaded")
   }
   if (!require(dplyr)) {
     stop("predict_sdm requires dplyr package, but it could not be loaded")
-  }
-  if (!require(sf)) {
-    stop("predict_sdm requires sf package, but it could not be loaded")
   }
   if (!require(dismo)) {
     stop("predict_sdm requires dismo package, but it could not be loaded")
@@ -80,23 +77,22 @@ predict_sdm <- function(nice_name,
   if (!file.exists(shapefile_name)) {
     unzip(zipfile = "data/gbif-shapefiles.zip")
   }
-  buffered_mcp <- sf::st_read(shapefile_name, quiet = TRUE)
+  buffered_mcp <- terra::vect(shapefile_name)
   
   # If necessary, adjust buffered MCP as appropriate - allowing larger buffers
   # for more distant time periods
   if (yr %in% c("2041", "2071")) {
     # Project buffered MCP to NA Albers Equal Area Conic 
-    buffered_mcp_proj <- sf::st_transform(buffered_mcp, crs = "ESRI:102008")
+    buffered_mcp_proj <- terra::project(buffered_mcp, "ESRI:102008")
     dist_mult <- dplyr::if_else(yr == "2041",
                                 true = 350,  # 350 km for 2041
                                 false = 900) # 900 km for 2071
     
     # Add to the buffer, based on appropriate distance multiplier
-    buffered_mcp <- sf::st_buffer(buffered_mcp_proj,
-                                  dist = dist_mult * 1000)
+    buffered_mcp <- terra::buffer(buffered_mcp_proj, width = dist_mult * 1000)
     
     # Transform back to lat/long
-    buffered_mcp <- sf::st_transform(buffered_mcp, 4326) 
+    buffered_mcp <- terra::project(buffered_mcp, "EPSG:4326") 
   }
   
   # Get list of climate variables that were considered for the SDM
@@ -108,16 +104,15 @@ predict_sdm <- function(nice_name,
                                   true = "data/wc2-1",
                                   false = paste0("data/ensemble/ssp", ssp, "/", yr))
   
-  predictors <- raster::stack(list.files(path = gcm_directory,
-                                         pattern = ".tif$",
-                                         full.names = TRUE))
+  predictors <- terra::rast(list.files(path = gcm_directory,
+                                       pattern = ".tif$",
+                                       full.names = TRUE))
   
   # Extract only those layers associated with climate variables in the model
   predictors <- terra::subset(predictors, climate_vars)
   
   # Crop and mask as appropriate (takes a few moments)
-  pred_mask <- raster::crop(predictors, buffered_mcp)
-  pred_mask <- raster::mask(pred_mask, buffered_mcp)
+  pred_mask <- terra::crop(predictors, buffered_mcp, mask = TRUE)
   
   # If using a lasso or gam model, need to standardize predictors using means 
   # and SDs from training dataset
@@ -142,15 +137,19 @@ predict_sdm <- function(nice_name,
   } else {
     # For lasso regression models from cv.glmnet: 
     # predict.cv.glmnet doesn't allow for rasters as input, so we need to
-    # convert pred_mask to a sparse matrix. Used code from Roozbeh Valavi: 
+    # convert pred_mask to a sparse matrix. Referenced code from Roozbeh Valavi: 
     # https://github.com/rvalavi/myspatial/blob/master/R/prediction.R
-    pred_points <- raster::rasterToPoints(pred_mask, spatial = TRUE)
-    data_sparse <- sparse.model.matrix(~. -1, pred_points@data)
-    pred_points@data$pred = as.numeric(predict(object = model, 
-                                               newx = data_sparse, 
-                                               type = "response", 
-                                               s = model$lambda.1se))
-    preds <- raster::rasterize(pred_points, pred_mask, field = "pred")
+    pred_points <- terra::as.points(pred_mask, 
+                                    values=TRUE, 
+                                    na.rm=TRUE, 
+                                    na.all=FALSE)
+    data_sparse <- sparse.model.matrix(~. -1, terra::values(pred_points))  
+    pred_vect <- as.numeric(predict(object = model, 
+                                    newx = data_sparse, 
+                                    type = "response", 
+                                    s = model$lambda.1se))
+    pred_points$pred <- pred_vect
+    preds <- terra::rasterize(pred_points, pred_mask, field = "pred")
   }
   
   # Send back this raster with the predicted values
