6f936d17718dd709b95c91bb75dca27ce722545b
Erin Zylstra <erinzylstra@gmail.com>
2023-08-18 17:04:46 -0700

    Use CV with LASSO models

--- development/src/butterfly-sdms-spatialCV.R
+++ development/src/butterfly-sdms-spatialCV.R
@@ -1,14 +1,14 @@
 # Running new maxent models for butterfly species
 # Erin Zylstra
 # ezylstra@arizona.edu
-# 2023-08-09
+# 2023-08-18
 
 require(stringr)
 require(ENMeval)
 require(raster)
 require(terra)
 require(dplyr)
-# require(ggplot2)
+require(glmnet)
 
 # Load up the functions from the functions folder
 source(file = "load_functions.R")
@@ -32,16 +32,17 @@ predictors <- terra::rast(list.files(path = "data/wc2-1",
                                      pattern = ".tif$",
                                      full.names = TRUE))
 
-# Get list of climate variables to consider for the SDM
-all_climate_vars <- read.csv("data/climate-variables.csv")
-climate_vars <- all_climate_vars$variable[all_climate_vars$include == TRUE]
+# For now, will use only 9 of the worldclim variables (this includes the 6 vars
+# that Low et al. 2020 used, plus a few varaibles that assess seasonal or daily
+# variation in temp/precip)
+climate_vars <- paste0("bio", c(1, 2, 4:6, 12:15))
 
-# Extract only those climate variables we need
+# Crop and mask predictor rasters
 predictors <- terra::subset(predictors, climate_vars)
 
 # Loop through species
 # for (i in 1:nrow(insect_data)) {
-i = 12
+i = 9
   nice_name <- nice_names[i]
 
   # Load in presence/absence data
@@ -52,8 +53,7 @@ i = 12
   if (!file.exists(pa_file)) {
     unzip(zipfile = "data/gbif-pa.zip")
   }
-  pa_data <- read.csv(file = pa_file) %>%
-    rename(old_fold = fold)
+  pa_data <- read.csv(file = pa_file)
   
   # Dataframes with lat/long (in that order) for occurrence and background locs
   occs <- pa_data %>%
@@ -91,16 +91,14 @@ i = 12
   
   # Visualize blocks
     # plot(pred_rs[[1]])
-    # points(y ~ x, data = filter(pa_data, block == 1 & pa == 1),  
+    # points(y ~ x, data = filter(pa_data, block == 1 & pa == 1),
     #        pch = 19, col = "red")
-    # points(y ~ x, data = filter(pa_data, block == 2 & pa == 1),  
+    # points(y ~ x, data = filter(pa_data, block == 2 & pa == 1),
     #        pch = 19, col = "blue")
-    # points(y ~ x, data = filter(pa_data, block == 3 & pa == 1),  
+    # points(y ~ x, data = filter(pa_data, block == 3 & pa == 1),
     #        pch = 19, col = "green")
-    # points(y ~ x, data = filter(pa_data, block == 4 & pa == 1),  
+    # points(y ~ x, data = filter(pa_data, block == 4 & pa == 1),
     #        pch = 19, col = "purple")
-  # Compare to ENMeval output:
-    # evalplot.grps(pts = occs, pts.grp = block$occs.grp, envs = pred_rs)
 
   # Maxent --------------------------------------------------------------------#
   # Set parameter values and arguments for ENMevaluate()
@@ -117,7 +115,7 @@ i = 12
   # For parallel processing, use two fewer cores than are available
   num_cores <- parallel::detectCores() - 2  
   
-  # Run maxent model:
+  # Run maxent models:
   mod_max <- ENMevaluate(occs = occs, 
                          bg = bg, 
                          envs = pred_rs,
@@ -129,24 +127,100 @@ i = 12
                          parallel = TRUE,
                          numCores = num_cores)
   
-  # Look at results
-  mod_max
-  mod_max@results
-  mod_max@results.partitions
-  # For each model, extract max or.10p.val and min AUC.val over 4 folds
-  partition_summary <- mod_max@results.partitions %>%
-    group_by(tune.args) %>%
-    summarize(auc.val.min = min(auc.val),
-              or.10p.max = max(or.10p)) %>%
-    data.frame()
+  model_eval <- evaluate_lasso(p = as.matrix(sdmtest[sdmtest$pa == 1, -1]), 
+                               a = as.matrix(sdmtest[sdmtest$pa == 0, -1]), 
+                               model = model_fit,
+                               s = model_fit$lambda.1se,
+                               type = "response")
   
-  # Pick best model (tuning parameters) and save evaluation metrics
+    # Look at results
+    # mod_max@results
+    # mod_max@results.partitions
+  
+  # Save to file
+  sdm_file <- paste0("development/output/SDMs/",
+                     nice_name, "-sdm-maxent-9var-CV.rds")
+  saveRDS(mod_max, sdm_file)
+  
+  # Prep data for all other SDMs ----------------------------------------------#  
+  predictors_df <- terra::extract(x = pred_mask,
+                                  y = pa_data[, c("x", "y")],
+                                  xy = FALSE) %>%
+    dplyr::select(-ID)
+  pa_data <- cbind(pa_data, predictors_df) 
+  
+  # LASSO ---------------------------------------------------------------------#
+  # Loop through each training dataset
+  for (j in 1:4) {
+    sdmtrain <- pa_data %>%
+      filter(block != j)
+    sdmtest <- pa_data %>%
+      filter(block == j)
+    
+    # Calculate means, SDs for standardizing covariates
+    stand_obj <- save_means_sds(sdmtrain, cols = climate_vars, verbose = TRUE)
+    # Standardize values in training dataset (to include quadratics, set quad = TRUE)
+    sdmtrain_preds <- prep_predictors(stand_obj, sdmtrain, quad = TRUE) 
+    sdmtrain <- cbind(pa = sdmtrain[,1], sdmtrain_preds)
+    
+    # Creating values to downweight background points (so total [summed] weight of 
+    # background points is equal to the total weight of presence points)
+    prNum <- sum(sdmtrain$pa == 1)
+    bgNum <- sum(sdmtrain$pa == 0)
+    wt <- ifelse(sdmtrain$pa == 1, 1, prNum / bgNum)
+    
+    # Run model
+    model_fit <- glmnet::cv.glmnet(x = as.matrix(sdmtrain[,2:ncol(sdmtrain)]),
+                                   y = sdmtrain$pa,
+                                   family = "binomial",
+                                   alpha = 1,
+                                   weights = wt,
+                                   standardize = FALSE)
+
+    # Need an easy way to calculate evaluation metrics:
+    
+    # Prep testing dataset
+    sdmtest_preds <- prep_predictors(stand_obj, sdmtest, quad = TRUE) 
+    sdmtest <- cbind(pa = sdmtest[,1], sdmtest_preds)
+
+    # Evaluate model performance with testing data
+    model_eval <- evaluate_lasso(p = as.matrix(sdmtest[sdmtest$pa == 1, -1]), 
+                                 a = as.matrix(sdmtest[sdmtest$pa == 0, -1]), 
+                                 model = model_fit,
+                                 s = model_fit$lambda.1se,
+                                 type = "response")
+    
+    library(flexsdm)
+    test <- sdm_eval(p = model_eval@presence,
+                     a = model_eval@absence,
+                     thr = "max_sens_spec")
+
+    
+    
+  }
+  
+
+  
+#------------------------------------------------------------------------------#
+# Finding best model and evaluating (just for Maxent right now)
+#------------------------------------------------------------------------------#
+  
+  # For each model, extract max or.10p.val and min AUC.val over 4 folds
+    partition_summary <- mod_max@results.partitions %>%
+      group_by(tune.args) %>%
+      summarize(auc.val.min = min(auc.val),
+                or.10p.max = max(or.10p),
+                cbi.val.min = min(cbi.val)) %>%
+      data.frame()
+    maxent_evals <- left_join(mod_max@results[, c(1:5, 8:9, 10:13, 16, 19)],
+                                partition_summary,
+                                by = "tune.args") %>%
+      relocate(or.10p.max, .after = or.10p.sd) %>%
+      relocate(auc.val.min, .after = auc.val.sd) %>%
+      relocate(cbi.val.min, .after = cbi.val.sd)
+
+  # Pick best maxent model (tuning parameters) and save evaluation metrics
   # Use minimum average or.10p (and maximum average auc.val to break ties)
-  maxent_evals <- left_join(mod_max@results[, c(1:5, 8:9, 12:13, 16, 19)],
-                            partition_summary,
-                            by = "tune.args") %>%
-    relocate(or.10p.max, .after = or.10p.sd) %>%
-    relocate(auc.val.min, .after = auc.val.sd)
   optimal <- maxent_evals %>% 
     filter(or.10p.avg == min(or.10p.avg)) %>%
     filter(auc.val.avg == max(auc.val.avg))
