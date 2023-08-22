d2800b58fbf8faef4c73fb25d944f9dbba3838e0
Erin Zylstra <erinzylstra@gmail.com>
2023-08-21 17:41:31 -0700

    Run all SDMs (except BRT) with CV

--- development/src/butterfly-sdms-spatialCV.R
+++ development/src/butterfly-sdms-spatialCV.R
@@ -1,14 +1,18 @@
 # Running new maxent models for butterfly species
 # Erin Zylstra
 # ezylstra@arizona.edu
-# 2023-08-18
+# 2023-08-21
 
 require(stringr)
 require(ENMeval)
 require(raster)
 require(terra)
 require(dplyr)
+require(flexsdm)
+require(ecospat)
 require(glmnet)
+require(mgcv)
+require(randomForest)
 
 # Load up the functions from the functions folder
 source(file = "load_functions.R")
@@ -40,10 +44,28 @@ climate_vars <- paste0("bio", c(1, 2, 4:6, 12:15))
 # Crop and mask predictor rasters
 predictors <- terra::subset(predictors, climate_vars)
 
+# Create table to store evaluation metrics for each SDM and fold
+insects <- insect_data$species
+sdms <- c("BRT", "GAM", "LASSO", "MAXENT", "RF")
+folds <- 1:4
+evals <- expand.grid(insect = insects, sdm = sdms, fold = folds,
+                     stringsAsFactors = FALSE) %>%
+  mutate(tune.args = NA,
+         AUC = NA,
+         CBI = NA,
+         IMAE = NA, # Inverse mean absolute error
+         thr.mss = NA, # Max(sens + spec) threshold value
+         OR.mss = NA, # OR, w/ thr = max(sens + spec)
+         TSS.mss = NA # True skill statistic, w/ thr = max(sens + spec)
+         ) %>%
+  relocate(tune.args, .after = sdm) %>%
+  arrange(insect, sdm, fold) %>%
+  data.frame()
+
 # Loop through species
-# for (i in 1:nrow(insect_data)) {
-i = 9
+for (i in 1:nrow(insect_data)) {
   nice_name <- nice_names[i]
+  insect <- insect_data$species[i]
 
   # Load in presence/absence data
   pa_file <- paste0("data/gbif/presence-absence/",
@@ -101,16 +123,32 @@ i = 9
     #        pch = 19, col = "purple")
 
   # Maxent --------------------------------------------------------------------#
-  # Set parameter values and arguments for ENMevaluate()
+  # Set tuning parameters for ENMevaluate()
   feature_classes <- c("L", "LQ", "H", "LQH")
   multipliers <- 1:3
   tune.args <- list(fc = feature_classes, rm = multipliers)
   user.grp <- list(occs.grp = pa_data$block[pa_data$pa == 1],
                    bg.grp = pa_data$block[pa_data$pa == 0])
+  
+  # Specify how AUC should be calculated. Assigning other.settings$validation.bg 
+  # to "partition" will calculate AUC with respect to the validation background 
+  # only (see Radosavljevic & Anderson 2014).
   os <- list(validation.bg = "partition")  
-  # Assigning other.settings$validation.bg to "partition" will calculate AUC 
-  # with respect to the validation background only 
-  # (see Radosavljevic & Anderson 2014). 
+
+  # Calculate a few other evaluation metrics, so I can compare model performance
+  # with other SDMs. Note that the CBI value ENMevaluate spits out is the same
+  # as what's produced with the ecospat package, with fit = predictions from
+  # occurrence and bg points in validation area, and nclass = 0.
+  em <- function(vars) {
+    em <- flexsdm::sdm_eval(p = vars$occs.val.pred, 
+                            a = vars$bg.val.pred,
+                            thr = "max_sens_spec")
+    out <- data.frame(IMAE = em$IMAE,
+                      TSS = em$TSS,
+                      OR.mss = em$OR,
+                      thr = em$thr_value)
+    return(out)
+  }
   
   # For parallel processing, use two fewer cores than are available
   num_cores <- parallel::detectCores() - 2  
@@ -124,20 +162,30 @@ i = 9
                          user.grp = user.grp,
                          tune.args = tune.args,
                          other.settings = os,
+                         user.eval = em,
                          parallel = TRUE,
                          numCores = num_cores)
-  
-  model_eval <- evaluate_lasso(p = as.matrix(sdmtest[sdmtest$pa == 1, -1]), 
-                               a = as.matrix(sdmtest[sdmtest$pa == 0, -1]), 
-                               model = model_fit,
-                               s = model_fit$lambda.1se,
-                               type = "response")
-  
     # Look at results
     # mod_max@results
     # mod_max@results.partitions
   
-  # Save to file
+  # Pick best maxent model (tuning parameters) and save evaluation metrics
+  # Use minimum average or.10p (and maximum average auc.val to break ties)
+  optimal <- mod_max@results %>% 
+    filter(or.10p.avg == min(or.10p.avg)) %>%
+    filter(auc.val.avg == max(auc.val.avg))
+  partitions <- mod_max@results.partitions %>%
+    filter(tune.args == optimal$tune.args)
+  eval_rows <- which(evals$insect == insect & evals$sdm == "MAXENT")
+  evals$tune.args[eval_rows] <- as.character(partitions$tune.args)
+  evals$AUC[eval_rows] <- partitions$auc.val
+  evals$CBI[eval_rows] <- partitions$cbi.val
+  evals$IMAE[eval_rows] <- partitions$IMAE  
+  evals$thr.mss[eval_rows] <- partitions$thr
+  evals$OR.mss[eval_rows] <- partitions$OR.mss
+  evals$TSS.mss[eval_rows] <- partitions$TSS
+
+  # Save ENMeval object to file (contains all 12 models w/diff tuning params)
   sdm_file <- paste0("development/output/SDMs/",
                      nice_name, "-sdm-maxent-9var-CV.rds")
   saveRDS(mod_max, sdm_file)
@@ -149,7 +197,7 @@ i = 9
     dplyr::select(-ID)
   pa_data <- cbind(pa_data, predictors_df) 
   
-  # LASSO ---------------------------------------------------------------------#
+  # Other SDMs ----------------------------------------------------------------#
   # Loop through each training dataset
   for (j in 1:4) {
     sdmtrain <- pa_data %>%
@@ -159,85 +207,131 @@ i = 9
     
     # Calculate means, SDs for standardizing covariates
     stand_obj <- save_means_sds(sdmtrain, cols = climate_vars, verbose = TRUE)
-    # Standardize values in training dataset (to include quadratics, set quad = TRUE)
-    sdmtrain_preds <- prep_predictors(stand_obj, sdmtrain, quad = TRUE) 
-    sdmtrain <- cbind(pa = sdmtrain[,1], sdmtrain_preds)
+    
+    # Standardize values in training, testing datasets with quadratics
+    sdmtrain_zq <- prep_predictors(stand_obj, sdmtrain, quad = TRUE) 
+    sdmtrain_zq <- cbind(pa = sdmtrain[,1], sdmtrain_zq)
+    sdmtest_zq <- prep_predictors(stand_obj, sdmtest, quad = TRUE) 
+    sdmtest_zq <- cbind(pa = sdmtest[,1], sdmtest_zq)
+    
+    # Standardize values in training, testing datasets without quadratics
+    sdmtrain_z <- prep_predictors(stand_obj, sdmtrain, quad = FALSE) 
+    sdmtrain_z <- cbind(pa = sdmtrain[,1], sdmtrain_z)
+    sdmtest_z <- prep_predictors(stand_obj, sdmtest, quad = FALSE) 
+    sdmtest_z <- cbind(pa = sdmtest[,1], sdmtest_z)
     
     # Creating values to downweight background points (so total [summed] weight of 
     # background points is equal to the total weight of presence points)
-    prNum <- sum(sdmtrain$pa == 1)
-    bgNum <- sum(sdmtrain$pa == 0)
+    prNum <- as.numeric(table(sdmtrain$pa)["1"])
+    bgNum <- as.numeric(table(sdmtrain$pa)["0"])
     wt <- ifelse(sdmtrain$pa == 1, 1, prNum / bgNum)
     
-    # Run model
-    model_fit <- glmnet::cv.glmnet(x = as.matrix(sdmtrain[,2:ncol(sdmtrain)]),
-                                   y = sdmtrain$pa,
-                                   family = "binomial",
-                                   alpha = 1,
-                                   weights = wt,
-                                   standardize = FALSE)
+    # Run GAM and save evaluation metrics
+      # Create model formula
+      smooth <- "s"
+      model_formula <- paste0(smooth, "(", climate_vars, "_1)")
+      model_formula <- paste(model_formula, collapse = " + ")
+      model_formula <- as.formula(paste0("pa ~ ", model_formula))  
+      # Model below adds a double penalty to remove variables that don't help
+      gam_fit <- mgcv::gam(model_formula,
+                           data = sdmtrain_z,
+                           family = binomial, 
+                           method = 'REML', 
+                           select = TRUE)
+      gam_eval <- dismo::evaluate(p = sdmtest_z[sdmtest_z$pa == 1,],
+                                  a = sdmtest_z[sdmtest_z$pa == 0,],
+                                  model = gam_fit,
+                                  type = "response")
+      gam_em <- flexsdm::sdm_eval(p = gam_eval@presence,
+                                  a = gam_eval@absence,
+                                  thr = "max_sens_spec")  
+      gam_boyce <- ecospat::ecospat.boyce(fit = c(gam_eval@presence, 
+                                                  gam_eval@absence),
+                                          obs = gam_eval@presence,
+                                          nclass = 0,
+                                          PEplot = FALSE)   
+      # Save evaluation metrics
+      row_index <- which(evals$insect == insect & evals$sdm == "GAM" & evals$fold == j)
+      evals$tune.args[row_index] <- NA
+      evals$AUC[row_index] <- gam_em$AUC
+      evals$CBI[row_index] <- gam_boyce$cor
+      evals$IMAE[row_index] <- gam_em$IMAE  
+      evals$thr.mss[row_index] <- gam_em$thr_value
+      evals$OR.mss[row_index] <- gam_em$OR
+      evals$TSS.mss[row_index] <- gam_em$TSS
+    
+    # Run LASSO and save evaluation metrics
+      lasso_fit <- glmnet::cv.glmnet(x = as.matrix(sdmtrain_zq[,2:ncol(sdmtrain_zq)]),
+                                     y = sdmtrain_zq$pa,
+                                     family = "binomial",
+                                     alpha = 1,
+                                     weights = wt,
+                                     standardize = FALSE)
+      lasso_eval <- evaluate_lasso(p = as.matrix(sdmtest_zq[sdmtest_zq$pa == 1, -1]), 
+                                   a = as.matrix(sdmtest_zq[sdmtest_zq$pa == 0, -1]), 
+                                   model = lasso_fit,
+                                   s = lasso_fit$lambda.1se,
+                                   type = "response")
+      lasso_em <- flexsdm::sdm_eval(p = lasso_eval@presence,
+                                    a = lasso_eval@absence,
+                                    thr = "max_sens_spec")  
+      lasso_boyce <- ecospat::ecospat.boyce(fit = c(lasso_eval@presence, 
+                                                    lasso_eval@absence),
+                                            obs = lasso_eval@presence,
+                                            nclass = 0,
+                                            PEplot = FALSE)
+      
+      # Save evaluation metrics
+      row_index <- which(evals$insect == insect & evals$sdm == "LASSO" & evals$fold == j)
+      evals$tune.args[row_index] <- "lambda.1se"
+      evals$AUC[row_index] <- lasso_em$AUC
+      evals$CBI[row_index] <- lasso_boyce$cor
+      evals$IMAE[row_index] <- lasso_em$IMAE  
+      evals$thr.mss[row_index] <- lasso_em$thr_value
+      evals$OR.mss[row_index] <- lasso_em$OR
+      evals$TSS.mss[row_index] <- lasso_em$TSS
+    
+    # Run RF and save evaluation metrics
+      # Create model formula
+      model_formula <- paste(climate_vars, collapse = " + ")
+      model_formula <- as.formula(paste0("pa ~ ", model_formula))
+      smpsize <- c("0" = prNum, "1" = prNum)
+      sdmtrain_f <- sdmtrain
+      sdmtrain_f$pa <- as.factor(sdmtrain_f$pa)
 
-    # Need an easy way to calculate evaluation metrics:
-    
-    # Prep testing dataset
-    sdmtest_preds <- prep_predictors(stand_obj, sdmtest, quad = TRUE) 
-    sdmtest <- cbind(pa = sdmtest[,1], sdmtest_preds)
+      # Run model
+      rf_fit <- randomForest::randomForest(formula = model_formula,
+                                           data = sdmtrain_f,
+                                           ntree = 1000,
+                                           sampsize = smpsize,
+                                           replace = TRUE)
+      # For RF, which is a classification model, need to use predict with type 
+      # = "prob" before dismo::evaluate()
+      rf_preds <- predict(rf_fit, sdmtest[,climate_vars], type = "prob")[, 2]
+      rf_eval <- dismo::evaluate(p = rf_preds[which(sdmtest$pa == 1)],
+                                 a = rf_preds[which(sdmtest$pa == 0)])
+      rf_em <- flexsdm::sdm_eval(p = rf_eval@presence,
+                                 a = rf_eval@absence,
+                                 thr = "max_sens_spec")  
+      rf_boyce <- ecospat::ecospat.boyce(fit = c(rf_eval@presence,
+                                                 rf_eval@absence),
+                                         obs = rf_eval@presence,
+                                         nclass = 0,
+                                         PEplot = FALSE)
 
-    # Evaluate model performance with testing data
-    model_eval <- evaluate_lasso(p = as.matrix(sdmtest[sdmtest$pa == 1, -1]), 
-                                 a = as.matrix(sdmtest[sdmtest$pa == 0, -1]), 
-                                 model = model_fit,
-                                 s = model_fit$lambda.1se,
-                                 type = "response")
-    
-    library(flexsdm)
-    test <- sdm_eval(p = model_eval@presence,
-                     a = model_eval@absence,
-                     thr = "max_sens_spec")
-
-    
-    
+      # Save evaluation metrics
+      row_index <- which(evals$insect == insect & evals$sdm == "RF" & evals$fold == j)
+      evals$tune.args[row_index] <- paste0("mtry = ",rf_fit$mtry)
+      evals$AUC[row_index] <- rf_em$AUC
+      evals$CBI[row_index] <- rf_boyce$cor
+      evals$IMAE[row_index] <- rf_em$IMAE  
+      evals$thr.mss[row_index] <- rf_em$thr_value
+      evals$OR.mss[row_index] <- rf_em$OR
+      evals$TSS.mss[row_index] <- rf_em$TSS
+      
+      # Not saving each of the models for now
   }
+}  
   
+write.table(evals, "clipboard", sep = "\t", row.names = FALSE)
 
-  
-#------------------------------------------------------------------------------#
-# Finding best model and evaluating (just for Maxent right now)
-#------------------------------------------------------------------------------#
-  
-  # For each model, extract max or.10p.val and min AUC.val over 4 folds
-    partition_summary <- mod_max@results.partitions %>%
-      group_by(tune.args) %>%
-      summarize(auc.val.min = min(auc.val),
-                or.10p.max = max(or.10p),
-                cbi.val.min = min(cbi.val)) %>%
-      data.frame()
-    maxent_evals <- left_join(mod_max@results[, c(1:5, 8:9, 10:13, 16, 19)],
-                                partition_summary,
-                                by = "tune.args") %>%
-      relocate(or.10p.max, .after = or.10p.sd) %>%
-      relocate(auc.val.min, .after = auc.val.sd) %>%
-      relocate(cbi.val.min, .after = cbi.val.sd)
-
-  # Pick best maxent model (tuning parameters) and save evaluation metrics
-  # Use minimum average or.10p (and maximum average auc.val to break ties)
-  optimal <- maxent_evals %>% 
-    filter(or.10p.avg == min(or.10p.avg)) %>%
-    filter(auc.val.avg == max(auc.val.avg))
-  best_model <- mod_max@models[[optimal$tune.args]]
-  
-  # Make predictions and extract values for occurrence and bg locations
-  preds <- predict(object = pred_rs,
-                   model = best_model,
-                   type = "cloglog")
-  est_occ <- extract(preds, pa_data[pa_data$pa == 1, c("x", "y")])
-  est_bg <- extract(preds, pa_data[pa_data$pa == 0, c("x", "y")])
-  # Use evaluate function to get confusion matrices and calculate threshold
-  # Note that AUC from dismo::evaluate() = auc.train in ENMeval output
-  ev <- dismo::evaluate(est_occ, est_bg)
-  thr <- dismo::threshold(ev, stat = "spec_sens")
-  
-  # Visualize
-  par(mfrow = c(2, 1))
-  plot(preds)
-  plot(preds > thr)
