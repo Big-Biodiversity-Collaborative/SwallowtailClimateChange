fbefe6985c3335330ffaf002d27bb57081ac2a61
Erin Zylstra <erinzylstra@gmail.com>
2023-08-25 15:53:03 -0700

    Save and summarize evaluation metrics from CV

--- development/src/butterfly-sdms-spatialCV.R
+++ development/src/butterfly-sdms-spatialCV.R
@@ -1,7 +1,7 @@
 # Running new maxent models for butterfly species
 # Erin Zylstra
 # ezylstra@arizona.edu
-# 2023-08-22
+# 2023-08-25
 
 require(stringr)
 require(ENMeval)
@@ -13,6 +13,7 @@ require(ecospat)
 require(glmnet)
 require(mgcv)
 require(randomForest)
+require(dismo)
 require(gbm)
 
 # Load up the functions from the functions folder
@@ -42,7 +43,7 @@ predictors <- terra::rast(list.files(path = "data/wc2-1",
 # variation in temp/precip)
 climate_vars <- paste0("bio", c(1, 2, 4:6, 12:15))
 
-# Crop and mask predictor rasters
+# Subset climate variables
 predictors <- terra::subset(predictors, climate_vars)
 
 # Create table to store evaluation metrics for each SDM and fold
@@ -176,15 +177,35 @@ for (i in 1:nrow(insect_data)) {
                          user.eval = em,
                          parallel = TRUE,
                          numCores = num_cores)
-    # Look at results
-    # mod_max@results
-    # mod_max@results.partitions
-  
+
   # Pick best maxent model (tuning parameters) and save evaluation metrics
-  # Use minimum average or.10p (and maximum average auc.val to break ties)
+  # Eliminating any model that has one or more folds with a negative CBI.
+  # Of those remaining, use minimum average or.10p (and maximum average auc.val 
+  # to break ties; similar to Kass et al. 2022)
+  other_summaries <- mod_max@results.partitions %>%
+    group_by(tune.args) %>%
+    summarize(cbi.min = min(cbi.val),
+              auc.min = min(auc.val),
+              n.partitions = length(fold)) %>%
+    data.frame()
+    # Occasionally, there are no results for one fold (maybe the model didn't
+    # run ok?). Should remove this model from consideration. 
   optimal <- mod_max@results %>% 
-    filter(or.10p.avg == min(or.10p.avg)) %>%
-    filter(auc.val.avg == max(auc.val.avg))
+    left_join(., other_summaries, by = "tune.args") %>%
+    filter(n.partitions == 4)
+  
+  if (max(optimal$cbi.min) <= 0) {
+    optimal <- optimal %>%
+      filter(cbi.min == max(cbi.min)) %>%
+      filter(or.10p.avg == min(or.10p.avg)) %>%
+      filter(auc.val.avg == max(auc.val.avg))
+  } else {
+    optimal <- optimal %>%
+      filter(cbi.min > 0) %>%
+      filter(or.10p.avg == min(or.10p.avg)) %>%
+      filter(auc.val.avg == max(auc.val.avg))
+  }
+  
   partitions <- mod_max@results.partitions %>%
     filter(tune.args == optimal$tune.args)
   eval_rows <- which(evals$insect == insect & evals$sdm == "MAXENT")
@@ -453,4 +474,245 @@ for (i in 1:nrow(insect_data)) {
       evals$TSS.mss[row_index] <- rf_em$TSS
   }
 }  
+
+# Save table with evaluation metrics
+# write.csv(evals, 
+#           file = "development/output/evals-CV-insect-SDMs.csv", 
+#           row.names = FALSE)
+
+# If run previously, load evals data.frame
+evals <- read.csv("development/output/evals-CV-insect-SDMs.csv", header = TRUE)
+
+# Run new BRT models with mean number of trees across folds:
+  brt <- filter(evals, sdm == "BRT") %>%
+    mutate(tree = as.numeric(str_sub(tune.args, 6, 9)),
+           lr = as.numeric(str_sub(tune.args, 21, nchar(tune.args))),
+           node = as.numeric(str_sub(tune.args, 16, 16)))
+  brt_sp <- brt %>%
+    group_by(insect) %>%
+    summarize(lr.min = min(lr),
+              lr.max = max(lr)) %>%
+    data.frame()
+  brt <- brt %>%
+    left_join(., brt_sp, by = "insect") %>%
+    filter(lr == lr.min)
+  brt_sp <- brt %>%
+    group_by(insect) %>%
+    summarize(tree = mean(tree),
+              lr = mean(lr),
+              node = mean(node)) %>%
+    data.frame()
+  
+  for (i in 1:nrow(insect_data)) {
+    nice_name <- nice_names[i]
+    insect <- insect_data$species[i]
+    
+    # Load in presence/absence data
+    pa_file <- paste0("data/gbif/presence-absence/",
+                      nice_name,
+                      "-pa.csv")
+    pa_data <- read.csv(file = pa_file)
+    
+    # Dataframes with lat/long (in that order) for occurrence and background locs
+    occs <- pa_data %>%
+      filter(pa == 1) %>%
+      select(x, y)
+    bg <- pa_data %>%
+      filter(pa == 0) %>%
+      select(x, y)
+    
+    # Get shapefile for geographic extent (to crop predictor rasters)
+    shapefile_name <- paste0("data/gbif/shapefiles/",
+                             nice_name, 
+                             "-buffered-mcp.shp")
+    buffered_mcp <- vect(shapefile_name)
+    
+    # Crop and mask predictor rasters
+    pred_mask <- terra::crop(predictors, buffered_mcp)
+    pred_mask <- terra::mask(pred_mask, buffered_mcp) 
+  
+    # Create spatial blocks with ENM eval (creates 4 blocks via lat/long
+    # with relatively even number of occurrences in each partition)
+    block <- get.block(occs, bg)
+    # Attach block numbers to pa data
+    pa_data$block <- c(block$occs.grp, block$bg.grp)
+    
+    # Prep predictor data
+    predictors_df <- terra::extract(x = pred_mask,
+                                    y = pa_data[, c("x", "y")],
+                                    xy = FALSE) %>%
+      dplyr::select(-ID)
+    pa_data <- cbind(pa_data, predictors_df)
+    # Remove any rows with NAs
+    anyNAs <- apply(pa_data[, climate_vars], 1, function(x) sum(is.na(x)))
+    if (max(anyNAs) > 0) {
+      pa_data <- pa_data[-which(anyNAs > 0),]
+    }
+    
+    # Set BRT parameters
+    tc <- brt_sp$node[i]
+    lr <- brt_sp$lr[i]
+    n_trees <- brt_sp$tree[i]
+    
+    for (j in 1:4) {
+      sdmtrain <- pa_data %>%
+        filter(block != j)
+      sdmtest <- pa_data %>%
+        filter(block == j)
+      try(
+        brt_fit <- dismo::gbm.fixed(data = sdmtrain,
+                                   gbm.x = 6:ncol(sdmtrain),
+                                   gbm.y = 1, 
+                                   family = "bernoulli",
+                                   tree.complexity = tc,
+                                   learning.rate = lr,
+                                   n.trees = n_trees,
+                                   bag.fraction = 0.75,
+                                   verbose = TRUE)
+      )
+      if (!is.null(brt_fit)) {
+        tune.args <- paste0("tree.", n_trees, "_node.", tc, "_lr.", lr)
+        brt_eval <- dismo::evaluate(p = sdmtest[sdmtest$pa == 1, climate_vars],
+                                    a = sdmtest[sdmtest$pa == 0, climate_vars],
+                                    model = brt_fit,
+                                    type = "response")
+        brt_em <- flexsdm::sdm_eval(p = brt_eval@presence,
+                                    a = brt_eval@absence,
+                                    thr = "max_sens_spec") 
+        brt_boyce <- ecospat::ecospat.boyce(fit = c(brt_eval@presence, 
+                                                    brt_eval@absence),
+                                            obs = brt_eval@presence,
+                                            nclass = 0,
+                                            PEplot = FALSE)      
+        # Save evaluation metrics
+        row_index <- which(evals$insect == insect & evals$sdm == "BRT" & evals$fold == j)
+        evals$tune.args[row_index] <- tune.args
+        evals$AUC[row_index] <- brt_em$AUC
+        evals$CBI[row_index] <- brt_boyce$cor
+        evals$IMAE[row_index] <- brt_em$IMAE  
+        evals$thr.mss[row_index] <- brt_em$thr_value
+        evals$OR.mss[row_index] <- brt_em$OR
+        evals$TSS.mss[row_index] <- brt_em$TSS   
+      }
+    }
+  }
+
+# Save new version of evals dataframe with BRT models that use the same tuning
+# parameters
+# write.csv(evals, 
+#           file = "development/output/evals-CV-insect-SDMs-meanBRT.csv", 
+#           row.names = FALSE)
+
+# -----------------------------------------------------------------------------#
+# Load MAXENT (ENMeval) objects for each species and compile results
+  for (i in 1:nrow(insect_data)) {
+    nice_name <- nice_names[i]
+    insect <- insect_data$species[i]
+    max <- readRDS(paste0("development/output/SDMs/",
+                          nice_name,
+                          "-sdm-maxent-9var-CV.rds"))
+    results <- max@results %>%
+      mutate(insect = insect) %>%
+      select(c(insect, tune.args, auc.train, cbi.train, 
+               auc.val.avg, auc.val.sd, cbi.val.avg, cbi.val.sd, IMAE.avg,
+               or.10p.avg, thr.avg, OR.mss.avg, TSS.avg, ncoef)) %>%
+      rename(thr = thr.avg) %>%
+      mutate(across(auc.train:TSS.avg, function(x) round(x, 3)))
+    partitions <- max@results.partitions
+    rownames(partitions) <- NULL
+    partitions <- partitions %>%
+      mutate(insect = insect) %>%
+      select(c(insect, tune.args, fold, auc.val, cbi.val, IMAE, or.10p, thr,
+               OR.mss, TSS)) %>%
+      mutate(across(auc.val:thr, function(x) round(x, 3)))
+    if (i == 1) {
+      maxent_results <- results
+      maxent_partitions <- partitions
+    } else {
+      maxent_results <- rbind(maxent_results, results)
+      maxent_partitions <- rbind(maxent_partitions, partitions)
+    }
+  }
+  
+  # Write maxent result tables to file:
+  # write.csv(maxent_results, 
+  #           "development/output/evals-maxent-insect.csv",
+  #           row.names = FALSE)
+  # write.csv(maxent_partitions, 
+  #           "development/output/evals-byfold-maxent-insect.csv",
+  #           row.names = FALSE)  
+  
+# -----------------------------------------------------------------------------#  
+# Summarize evaluation metrics across SDMs and species
+  
+# If run previously, load evals data.frame with new BRT models
+evals <- read.csv("development/output/evals-CV-insect-SDMs-meanBRT.csv", 
+                  header = TRUE)
+
+# Identify problematic evaluation metrics:
+evals <- evals %>%
+  mutate(prob.AUC = ifelse(AUC < 0.5, 1, 0),
+         prob.CBI = ifelse(CBI < 0, 1, 0),
+         prob = ifelse(prob.AUC + prob.CBI > 0, 1, 0))
+
+# Summary of CBI problems:
+  cbi <- evals %>%
+    filter(prob.CBI == 1) %>%
+    count(., insect, sdm)
+  cbi.issues <- data.frame(insect = insect_data$species, 
+                           BRT = 0, GAM = 0, LASSO = 0, MAXENT = 0, RF = 0)
+  for (i in 1:nrow(cbi)) {
+    cbi.issues[cbi.issues$insect == cbi$insect[i], cbi$sdm[i]] <- 1
+  }
+  cbi.issues$n.SDMs <- 5 - rowSums(cbi.issues[, -1])
+  cbi.issues
+  apply(cbi.issues[, 2:6], 2, sum)
+  # Remember: this is just with 9 climate variables
+
+# Correlations between evaluation metrics
+round(cor(evals[, c("AUC", "CBI", "IMAE", "OR.mss", "TSS.mss")]), 2)
+plot(evals[, c("AUC", "CBI", "IMAE", "OR.mss", "TSS.mss")])
+# AUC and maxTSS highly correlated (0.95)
+# AUC and CBI correlated (0.58)
+# CBI and maxTSS weakly correlated (0.44)
+# IMAE isn't correlated with others (-0.17 < r 0.22)
+# OR isn't correlated with others (-0.33 < r < 0.04)
+
+# Could remove TSS without losing much information:
+round(cor(evals[, c("AUC", "CBI", "IMAE", "OR.mss")]), 2)
+plot(evals[, c("AUC", "CBI", "IMAE", "OR.mss")])
+
+evals_avg <- evals %>%
+  group_by(insect, sdm) %>%
+  summarize(auc.avg = mean(AUC),
+            auc.min = min(AUC),
+            cbi.avg = mean(CBI),
+            cbi.min = min(CBI),
+            imae.avg = mean(IMAE),
+            imae.min = min(IMAE),
+            thr.avg = mean(thr.mss),
+            or.avg = mean(OR.mss),
+            or.max = max(OR.mss),
+            .groups = "keep") %>%
+  mutate(across(auc.avg:or.max, function(x) round(x, 3))) %>%
+  data.frame()
+
+# Look at distributions of evaluation metrics (across species and models)
+par(mfrow = c(2, 1))
+hist(evals_avg$auc.avg, breaks = 20)
+abline(v = 0.5, col = "blue", lty = 2)
+hist(evals_avg$auc.min, breaks = 20)
+abline(v = 0.5, col = "blue", lty = 2)
+
+hist(evals_avg$cbi.avg, breaks = 20)
+abline(v = 0, col = "blue", lty = 2)
+hist(evals_avg$cbi.min, breaks = 20)
+abline(v = 0, col = "blue", lty = 2)
+
+hist(evals_avg$imae.avg, breaks = 20)
+hist(evals_avg$imae.min, breaks = 20)
+
+hist(evals_avg$or.avg, breaks = 20)
+hist(evals_avg$or.max, breaks = 20)
+
 
