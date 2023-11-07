e4093dca868ff3aa40d26a5d6ae8c19cdd01bf17
Jeff Oliver <jcoliver@email.arizona.edu>
2023-11-07 16:51:29 -0700

    Add removal of data files before extraction

--- src/hpc/prep-extract-data.R
+++ src/hpc/prep-extract-data.R
@@ -3,8 +3,24 @@
 # jcoliver@arizona.edu
 # 2023-10-20
 
+# Clean out old pa data files
+pa_files <- list.files(path = "data/gbif/presence-absence", 
+                       full.names = TRUE, 
+                       pattern = "*.csv")
+if (!all(file.remove(pa_files))) {
+  message("One or more presence absence files not removed")
+}
+
 # Extract data files
 unzip(zipfile = "data/gbif-pa.zip", overwrite = TRUE)
+
+# Clean out old shapefiles
+shape_files <-list.files(path = "data/gbif/shapefiles", 
+                         full.names = TRUE, 
+                         pattern = "*-buffered-mcp*")
+if (!all(file.remove(shape_files))) {
+  message("One or more shapefiles not removed")
+}
 
 # Extract shape files
 unzip(zipfile = "data/gbif-shapefiles.zip", overwrite = TRUE)
