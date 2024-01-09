de430cf9b5df0ba77cb08229616fb35445c326b4
Jeff Oliver <jcoliver@email.arizona.edu>
2024-01-09 13:18:40 -0700

    First draft of richness for insect-only predictions

--- src/summary/summary-3-draw-species-richness-maps.R
+++ src/summary/summary-3-draw-species-richness-maps.R
@@ -1,4 +1,4 @@
-# Create species richness maps of current & forecast insect species
+# Create insect species richness rasters & maps of current & forecast models
 # Jeff Oliver & Erin Zylstra
 # jcoliver@arizona.edu; ezylstra@arizona.edu
 # 2021-06-06
@@ -8,6 +8,13 @@ require(dplyr)
 require(stringr)
 require(ggplot2)
 source(file = "load_functions.R")
+
+# Two versions of rasters (and maps) will be created:
+# 1. Based on overlaps of insect and host (where predicted "suitable" cells 
+#    require not only presence of the insect, but also the presence of at least
+#    one host plant). Output files will have the suffix "ov".
+# 2. Based on insect predicted distribution alone (no plant information). 
+#    Output files will have the suffix "io".
 
 # Logical indicating whether to replace an overlap raster/map if one already 
 # exists
@@ -50,19 +57,30 @@ nice_names <- insects %>%
   str_replace(pattern = " ", replacement = "_") %>%
   tolower()
 
-# Will need to update raster values:
+# Will need to update raster values
+# For maps considering only areas where insect overlaps with at least one plant
+# species (inclusive)
 #   {0, 3}: 0
 #   {4, 5}: 1
-# Reclassification matrix, 
-rcl <- matrix(data = c(0, 3, 0, 
+# Reclassification matrix, for overlap maps
+ov_rcl <- matrix(data = c(0, 3, 0, 
                        3.1, 5, 1), # Bit of a cludge to get classify to work
+              nrow = 2,
+              byrow = TRUE)
+
+# For maps only considering insects
+#  {0, 2}: 0
+#  {3, 5}: 1
+# Reclassification matrix, for insect-only maps
+io_rcl <- matrix(data = c(0, 2, 0, 
+                       2.1, 5, 1), # Bit of a cludge to get classify to work
               nrow = 2,
               byrow = TRUE)
 
 # Loop over all climate models (including current)
 # Read in all insect rasters
 # Convert rasters to 0/1
-# Extend all rasters to same extent
+# ~Extend all rasters to same extent~ No need - handled by terra.
 # Sum all rasters together
 # Save raster to output/maps
 # If save_maps is TRUE, create ggplot map and save to output/maps
@@ -74,11 +92,17 @@ for (clim_model in climate_models$name) {
   output_basename <- paste0("output/richness/",
                             clim_model, 
                             "-richness")
-  richness_ras_filename <- paste0(output_basename, ".rds")
-  richness_map_filename <- paste0(output_basename, ".", file_ext)
+  # Output files for overlap richness (i.e. those including plants)
+  ov_richness_ras_filename <- paste0(output_basename, "-ov.rds")
+  ov_richness_map_filename <- paste0(output_basename, "-ov.", file_ext)
+  # Output files for insect only richness (not considering plants)
+  io_richness_ras_filename <- paste0(output_basename, "-io.rds")
+  io_richness_map_filename <- paste0(output_basename, "-io.", file_ext)
   
   # Check to see if files exist and only replace if necessary
-  if (!file.exists(richness_ras_filename) | replace) {
+  if (!base::all(file.exists(ov_richness_ras_filename), 
+                 file.exists(io_richness_ras_filename)) | 
+      replace) {
     overlap_files <- paste0("output/overlaps/",
                             nice_names, 
                             "-overlap-", 
@@ -99,58 +123,104 @@ for (clim_model in climate_models$name) {
     }
     # All rasters in a list
     raster_list <- lapply(X = overlap_files, FUN = readRDS)
+    # Overlap versions
     # Convert to 0/1
-    binary_rasters <- lapply(X = raster_list,
-                             FUN = terra::classify,
-                             rcl = rcl,
-                             include.lowest = TRUE)
+    ov_binary_rasters <- lapply(X = raster_list,
+                                FUN = terra::classify,
+                                rcl = ov_rcl,
+                                include.lowest = TRUE)
     # Add them all together, starting with a SpatRasterCollection, which turns 
     # out to be easier to work with (don't have to worry about modifying extents 
     # beforehand)
-    binary_coll <- terra::sprc(binary_rasters)
-    richness_ras <- terra::mosaic(x = binary_coll, fun = "sum")
-    saveRDS(object = richness_ras, file = richness_ras_filename)
+    ov_binary_coll <- terra::sprc(ov_binary_rasters)
+    ov_richness_ras <- terra::mosaic(x = ov_binary_coll, fun = "sum")
+    saveRDS(object = ov_richness_ras, file = ov_richness_ras_filename)
+
+    # Insect only version
+    # Convert to 0/1
+    io_binary_rasters <- lapply(X = raster_list,
+                                FUN = terra::classify,
+                                rcl = io_rcl,
+                                include.lowest = TRUE)
+    # Add them all together
+    io_binary_coll <- terra::sprc(io_binary_rasters)
+    io_richness_ras <- terra::mosaic(x = io_binary_coll, fun = "sum")
+    saveRDS(object = io_richness_ras, file = io_richness_ras_filename)
     
     # Create map (image file) if necessary
     if (save_maps) {
-      rich_map <- richness_map(r = richness_ras,
+      # Overlap version
+      ov_rich_map <- richness_map(r = ov_richness_ras,
                                predictor = clim_model,
                                direction = -1)
-      ggsave(filename = richness_map_filename,
-             plot = rich_map)
+      ggsave(filename = ov_richness_map_filename,
+             plot = ov_rich_map)
+      # Insect-only version
+      io_rich_map <- richness_map(r = io_richness_ras,
+                                  predictor = clim_model,
+                                  direction = -1)
+      ggsave(filename = io_richness_map_filename,
+             plot = io_rich_map)
     }
 
     # If this is not current climate, do comparison with contemporary richness 
     # to create a delta richness map
     if (clim_model != "current") {
-      current_file <- "output/richness/current-richness.rds"
-      if (file.exists(current_file)){
+      ov_current_file <- "output/richness/current-richness-ov.rds"
+      io_current_file <- "output/richness/current-richness-io.rds"
+      # Only proceed if both current richness files are available
+      if (all(file.exists(ov_current_file), file.exists(io_current_file))){
         # Destination files
         delta_basename <- paste0("output/richness/",
                                  clim_model, 
                                  "-delta-richness")
-        delta_ras_filename <- paste0(delta_basename, ".rds")
-        delta_map_filename <- paste0(delta_basename, ".", file_ext)
-
+        # Output for overlap deltas
+        ov_delta_ras_filename <- paste0(delta_basename, "-ov.rds")
+        ov_delta_map_filename <- paste0(delta_basename, "-ov.", file_ext)
+        # Output for insect-only deltas
+        io_delta_ras_filename <- paste0(delta_basename, "-io.rds")
+        io_delta_map_filename <- paste0(delta_basename, "-io.", file_ext)
+        
+        # Overlap version
         # Load contemporary richness raster
-        current_richness <- readRDS(file = current_file)
+        ov_current_richness <- readRDS(file = ov_current_file)
         # Create a SpatRasterCollection from raster list, changing the current 
         # raster to negative values so we can use the sum function in 
         # terra::mosaic, i.e. Forecast richness - Current richness.
-        delta_coll <- terra::sprc(list((-1) * current_richness, richness_ras))
+        ov_delta_coll <- terra::sprc(list((-1) * ov_current_richness, ov_richness_ras))
         # Calculate delta of rasters
-        delta_ras <- terra::mosaic(x = delta_coll, fun = "sum")
+        ov_delta_ras <- terra::mosaic(x = ov_delta_coll, fun = "sum")
         # Save delta raster
-        saveRDS(object = richness_ras, file = delta_ras_filename)
+        saveRDS(object = ov_delta_ras, file = ov_delta_ras_filename)
+
+        # Insect only version
+        # Load contemporary richness raster
+        io_current_richness <- readRDS(file = io_current_file)
+        # Create a SpatRasterCollection from raster list, changing the current 
+        # raster to negative values so we can use the sum function in 
+        # terra::mosaic, i.e. Forecast richness - Current richness.
+        io_delta_coll <- terra::sprc(list((-1) * io_current_richness, io_richness_ras))
+        # Calculate delta of rasters
+        io_delta_ras <- terra::mosaic(x = io_delta_coll, fun = "sum")
+        # Save delta raster
+        saveRDS(object = io_delta_ras, file = io_delta_ras_filename)
         
-        # If appropriate, save delta map
+        # If appropriate, save delta maps
         if (save_maps) {
-          delta_map <- richness_map(r = delta_ras,
+          # Overlap
+          ov_delta_map <- richness_map(r = ov_delta_ras,
                                     predictor = clim_model,
                                     palette = "purple",
                                     legend = "Richness change")
-          ggsave(filename = delta_map_filename,
-                 plot = delta_map)
+          ggsave(filename = ov_delta_map_filename,
+                 plot = ov_delta_map)
+          # Insect-only
+          io_delta_map <- richness_map(r = io_delta_ras,
+                                       predictor = clim_model,
+                                       palette = "purple",
+                                       legend = "Richness change")
+          ggsave(filename = io_delta_map_filename,
+                 plot = io_delta_map)
         }
       } else {
         message("No current richness rasters on disk; deltas not created.")
