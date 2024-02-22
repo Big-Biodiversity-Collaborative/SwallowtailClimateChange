7a3a5702ee597bb82c02778ee003cf000cbcbe8e
Jeff Oliver <jcoliver@email.arizona.edu>
2024-02-22 11:27:39 -0700

    FLTOG, fix placeholder function so it stops erroring

--- functions/run_one_SDMs_full.R
+++ functions/run_one_SDMs_full.R
@@ -2,12 +2,6 @@
 
 # TODO: Convert to function for use by run-all-2-SDMs-full.R script
 
-run_one_SDMs_full(species_name, rerun = TRUE, num_cores = 2) {
- 
+run_one_SDMs_full <- function(species_name, rerun = TRUE, num_cores = 2) {
   
-  
-  
-  
-  
-   
 }
