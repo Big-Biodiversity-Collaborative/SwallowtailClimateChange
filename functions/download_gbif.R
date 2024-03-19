efb861f6db7dba05f89ce3cb782d0cdc8a013bcc
Jeff Oliver <jcoliver@email.arizona.edu>
2024-03-18 20:30:42 -0700

    Add functionality for verbatimSpeciesName query

--- functions/download_gbif.R
+++ functions/download_gbif.R
@@ -2,22 +2,25 @@
 #' 
 #' @param species_name the name of the species for record keeping
 #' @param gbif_name the name to query GBIF for; not necessarily the same as 
-#' \code{species_name} due to GBIF taxon name problems
-#' @param replace logical indicating whether or not to overwrite data for this 
-#' species that may already exist on disk
+#' \code{species_name} due to GBIF taxon name problems; ignored if 
+#' \code{verbatim_name} is not null
+#' @param verbatim_name the name of the species on GBIF; only used in cases 
+#' where the species name is not an accepted name in GBIF backbone taxonomy;
+#' @param replace DEPRECATED logical indicating whether or not to overwrite 
+#' data for this species that may already exist on disk; function no longer 
+#' writes data to file, instead returns data.frame of observations (see 
+#' \textbf{Value})
 #' @param verbose logical indicating whether or not to print progress messages
 #' @param countries character vector of country codes to restrict search 
 #' @param query_limit integer number of results to return per query
 #' @param max_attempts integer maximum number of times to try querying gbif
 #' @param logfile character name of file to write details on failed queries
 #' 
-#' @details Downloaded data are downloaded to the 'data' directory with the 
-#' following file naming convention: <genus name>_<specific epithet>-gbif.csv.
-#' 
-#' @return NULL
-download_gbif <- function(species_name, gbif_name, replace = FALSE, 
-                          verbose = FALSE, countries = c("CA", "MX", "US"),
-                          query_limit = 200, max_attempts = 5, logfile = NULL) {
+#' @return tibble of observations
+download_gbif <- function(species_name, gbif_name = NULL, verbatim_name = NULL, 
+                          replace = FALSE, verbose = FALSE, 
+                          countries = c("CA", "MX", "US"), query_limit = 200, 
+                          max_attempts = 5, logfile = NULL) {
   if (!require(dplyr)) {
     stop("download_gbif requires dplyr package, but it could not be loaded")
   }
@@ -27,215 +30,227 @@ download_gbif <- function(species_name, gbif_name, replace = FALSE,
   if (!require(stringr)) {
     stop("download_gbif requires stringr package, but it could not be loaded")
   }
-
-  nice_name <- tolower(x = gsub(pattern = " ",
-                                replacement = "_",
-                                x = species_name))
-  filename <- paste0("data/gbif/downloaded/", nice_name, "-gbif.csv")
   
-  # Only proceed if file doesn't exist or we want to replace existing files
-  if (!file.exists(filename) | replace) {
-    if (verbose) {
-      message(paste0("\n****   Beginning process for ", species_name, "  ****"))
-    }
-    
+  if (verbose) {
+    message(paste0("\n****   Beginning process for ", species_name, "  ****"))
+  }
+  
+  # The name of the list element with occurrence records will vary: if we 
+  # pass a name to the query parameter (i.e. the gbif_name), it takes the 
+  # same string, but replaces spaces with underscores; if we instead do not 
+  # pass anything through the query parameter, but rather through the 
+  # verbatimScientificName element of the gbifopts list (i.e. the 
+  # verbatim_name), it just uses the string "custom_query" as the name of the 
+  # list element
+  
+  if (is.null(verbatim_name)) {
     # We use the gbif name to extract results from list object, but spocc
     # replaces spaces with underscores for list element names
     list_name <- gsub(pattern = " ",
                       replacement = "_",
                       x = gbif_name)
-    # To restrict to three countries, need to do three separate queries
-    total <- 0
-    obs <- NULL
-    for (country in countries) {
-      if (verbose) {
-        message(paste0("Counting GBIF records for ", 
-                       species_name, " (as ", gbif_name, ") from ",
-                       country))
-      }
-      # Start with query just to count number of records, so we know how many 
-      # pages of results we'll need to request
+  } else { # when searching for verbatim_name
+    list_name <- "custom_query"
+  }
+  # To restrict to three countries, need to do three separate queries
+  total <- 0
+  obs <- NULL
+  for (country in countries) {
+    # query_as_name the name we use for reporting messages out
+    if (is.null(verbatim_name)) { # because if_else is too cranky
+      query_as_name <- gbif_name
+    } else {
+      query_as_name <- verbatim_name
+    }
+    if (verbose) {
+      message(paste0("Counting GBIF records for ", 
+                     species_name, " (as ", query_as_name, ") from ",
+                     country))
+    }
+    # Start with query just to count number of records, so we know how many 
+    # pages of results we'll need to request
+    
+    # Query parameters differ based on whether we are using a name accepted 
+    # by gbif (gbif_name) or a name not accepted by gbif (verbatim_name)
+    if (is.null(verbatim_name)) {
       gbif_count <- spocc::occ(query = gbif_name,
                                from = "gbif",
                                limit = 1,
                                has_coords = TRUE,
                                gbifopts = list(country = country))
+    } else {
+      gbif_count <- spocc::occ(from = "gbif",
+                               limit = 1,
+                               has_coords = TRUE,
+                               gbifopts = list(country = country,
+                                               verbatimScientificName = verbatim_name))
+    }
+    
+    num_records <- gbif_count$gbif$meta$found
+    total <- total + num_records
+    
+    if (num_records > 0) {
+      starts <- seq(from = 0, 
+                    to = num_records,
+                    by = query_limit)
       
-      num_records <- gbif_count$gbif$meta$found
-      total <- total + num_records
+      # Perform multiple queries, paging over results
+      if (verbose) {
+        message(paste0("Downloading ", num_records, " records of ", 
+                       species_name, " (as ", query_as_name, ") from ",
+                       country))
+      }
       
-      if (num_records > 0) {
-        starts <- seq(from = 0, 
-                      to = num_records,
-                      by = query_limit)
-        
-        # Perform multiple queries, paging over results
-        if (verbose) {
-          message(paste0("Downloading ", num_records, " records of ", 
-                         species_name, " (as ", gbif_name, ") from ",
-                         country))
-        }
-
-        for (start in starts) {
-          # For edge case, since gbif record indexing actually starts at 0
-          if (start < num_records) {
-            if (verbose) {
-              # For reporting, add one to deal with 0-indexing
-              end <- min((start + query_limit), num_records)
-              message(paste0("...records ", (start + 1), " through ", end, " of ", num_records))
-            }
-
-            # Sometimes GBIF times out, so we will keep track of how many 
-            # attempts we make and stop at max_attempts
-            success <- FALSE
-            attempts <- 0
-            while(!success && (attempts < max_attempts)) {
+      for (start in starts) {
+        # For edge case, since gbif record indexing actually starts at 0
+        if (start < num_records) {
+          if (verbose) {
+            # For reporting, add one to deal with 0-indexing
+            end <- min((start + query_limit), num_records)
+            message(paste0("...records ", (start + 1), " through ", end, " of ", num_records))
+          }
+          
+          # Sometimes GBIF times out, so we will keep track of how many 
+          # attempts we make and stop at max_attempts
+          success <- FALSE
+          attempts <- 0
+          while(!success && (attempts < max_attempts)) {
+            tryCatch(expr = {
+              attempts <- attempts + 1
+              # If last attempt failed, sleep briefly
+              if (attempts > 1) {
+                Sys.sleep(runif(n = 1, min = 1, max = 2))
+              }
               
-              ### Start new try/catch
-              tryCatch(expr = {
-                attempts <- attempts + 1
-                # If last attempt failed, sleep briefly
-                if (attempts > 1) {
-                  Sys.sleep(runif(n = 1, min = 1, max = 2))
-                }
-                
+              if (is.null(verbatim_name)) {
                 gbif_query <- spocc::occ(query = gbif_name,
                                          from = "gbif",
                                          limit = query_limit,
                                          start = start,
                                          has_coords = TRUE,
                                          gbifopts = list(country = country))
-                
-                query_data <- gbif_query$gbif$data[[list_name]]
-                
-                # Failed queries (in these cases) are characterized by a set of 
-                # data that lacks the countryCode column. We use that as an 
-                # indicator of success
-                if ("countryCode" %in% colnames(query_data)) {
-                  success <- TRUE
-                } else {
-                  if (verbose & attempts < max_attempts) {
-                    message(paste0("\tquery failed on attempt ", attempts, ", retrying."))
-                  }
-                }
-              }, # End of expression to try
-              error = function(e) {
-                message("...unsuccessful query on attempt ", attempts, " of ", 
-                        max_attempts)
-                e
-              }, # End of error catching
-              finally = NULL) # end of tryCatch
-              #### End new
-
-              #### Start prior approach (no try/catch)
-              # gbif_query <- spocc::occ(query = gbif_name,
-              #                          from = "gbif",
-              #                          limit = query_limit,
-              #                          start = start,
-              #                          has_coords = TRUE,
-              #                          gbifopts = list(country = country))
-              # 
-              # query_data <- gbif_query$gbif$data[[list_name]]
-              # attempts <- attempts + 1
+              } else {
+                gbif_query <- spocc::occ(from = "gbif",
+                                         limit = query_limit,
+                                         start = start,
+                                         has_coords = TRUE,
+                                         gbifopts = list(country = country,
+                                                         verbatimScientificName = verbatim_name))
+              }
+              
+              query_data <- gbif_query$gbif$data[[list_name]]
               
               # Failed queries (in these cases) are characterized by a set of 
               # data that lacks the countryCode column. We use that as an 
               # indicator of success
-              # if ("countryCode" %in% colnames(query_data)) {
-              #   success <- TRUE
-              # } else {
-              #   if (verbose & attempts < max_attempts) {
-              #     message(paste0("\tquery failed on attempt ", attempts, ", retrying."))
-              #   }
-              # }
-              #### End prior
-
-            } # end while loop
-            if (success) {
-              # Extract the data and clean it up before adding to results
-              # spocc::occ returns list of data frames, indexed by query
-              query_data <- query_data %>%
-                dplyr::filter(countryCode %in% c("CA", "MX", "US"),
-                              stringr::str_detect(issues, pattern = "ZERO_COORDINATE", 
-                                                  negate = TRUE))
-              
-              # If any data remain, add to results
-              if (nrow(query_data) > 0) {
-                if (is.null(obs)) {
-                  obs <- query_data
-                } else {
-                  obs <- obs %>%
-                    dplyr::bind_rows(query_data)
+              if ("countryCode" %in% colnames(query_data)) {
+                success <- TRUE
+              } else {
+                if (verbose & attempts < max_attempts) {
+                  message(paste0("\tquery failed on attempt ", attempts, ", retrying."))
                 }
               }
-            } else { # never had a successful query
-              # Want to make a note of this in the logfile (if one exists)
-              if (!is.null(logfile)) {
-                if (file.exists(logfile)) {
-                  sink(file = logfile, append = TRUE)
-                  cat("\nFailed query for ", species_name, " (as ", 
-                      gbif_name, ") from ", country, ", records ", 
-                      (start + 1), "-", end, 
-                      "; dataset may be incomplete.", sep = "")
-                  sink()
-                }
-              }
-              
-              if (verbose) {
-                message(paste0("\tquery failed after ", attempts, " attempts"))
+            }, # End of expression to try
+            error = function(e) {
+              message("...unsuccessful query on attempt ", attempts, " of ", 
+                      max_attempts)
+              e
+            }, # End of error catching
+            finally = NULL) # end of tryCatch
+            
+          } # end while loop
+          
+          if (success) {
+            # Extract the data and clean it up before adding to results
+            # spocc::occ returns list of data frames, indexed by query
+            query_data <- query_data %>%
+              dplyr::filter(countryCode %in% c("CA", "MX", "US"),
+                            stringr::str_detect(issues, pattern = "ZERO_COORDINATE", 
+                                                negate = TRUE))
+            
+            # If any data remain, add to results
+            if (nrow(query_data) > 0) {
+              if (is.null(obs)) {
+                obs <- query_data
+              } else {
+                obs <- obs %>%
+                  dplyr::bind_rows(query_data)
               }
             }
-          } # end conditional for start < num_records
-        } # end iteration over starts
-      } else {
-        if (verbose) {
-          message(paste0("Zero records of ", species_name, 
-                         " found from ", country))
-        }
-      }
-    } # End iterating over all countries
-
-    # Ready to do final data prep and write to disk
-    if (!is.null(obs)) {    
-      # Want to make sure that, before we write to file, we have all the standard 
-      # columns. It is possible to have results that lack date columns (yes, so 
-      # much fun), so add them in if necessary; otherwise select fails
-      if (!("year" %in% colnames(obs))) {
-        obs <- obs %>%
-          dplyr::mutate(year = NA)
-      }
-      if (!("month" %in% colnames(obs))) {
-        obs <- obs %>%
-          dplyr::mutate(month = NA)
-      }
-      if (!("day" %in% colnames(obs))) {
-        obs <- obs %>%
-          dplyr::mutate(day = NA)
-      }
-      
-      # Select only those columns we want to write to file
-      obs <- obs %>%
-        dplyr::select(gbifID, species, longitude, latitude, 
-                      year, month, day, countryCode, basisOfRecord)
-      
-      # Before writing to file, add a column with accepted name (which
-      # may be different than what GBIF puts in species column)
-      obs$accepted_name <- species_name
-
-      # Write to file and report back (if appropriate)
-      write.csv(x = obs,
-                file = filename,
-                row.names = FALSE)
-      
-      if (verbose) {
-        message(paste0(nrow(obs), " records of ", species_name, 
-                       " written to ", filename))
-      }
+          } else { # never had a successful query
+            # Want to make a note of this in the logfile (if one exists)
+            if (!is.null(logfile)) {
+              if (file.exists(logfile)) {
+                sink(file = logfile, append = TRUE)
+                cat("\nFailed query for ", species_name, " (as ", 
+                    gbif_name, ") from ", country, ", records ", 
+                    (start + 1), "-", end, 
+                    "; dataset may be incomplete.", sep = "")
+                sink()
+              }
+            }
+            
+            if (verbose) {
+              message(paste0("\tquery failed after ", attempts, " attempts"))
+            }
+          }
+        } # end conditional for start < num_records
+      } # end iteration over starts
     } else {
       if (verbose) {
-        message(paste0("After filtering, no records of ", species_name,
-                       " saved to disk."))
+        message(paste0("Zero records of ", species_name, 
+                       " found from ", country))
       }
+    }
+  } # End iterating over all countries
+  
+  # Ready to do final data prep and write to disk
+  if (!is.null(obs)) {    
+    # Want to make sure that, before we write to file, we have all the standard 
+    # columns. It is possible to have results that lack date columns (yes, so 
+    # much fun), so add them in if necessary; otherwise select fails
+    if (!("year" %in% colnames(obs))) {
+      obs <- obs %>%
+        dplyr::mutate(year = NA)
+    }
+    if (!("month" %in% colnames(obs))) {
+      obs <- obs %>%
+        dplyr::mutate(month = NA)
+    }
+    if (!("day" %in% colnames(obs))) {
+      obs <- obs %>%
+        dplyr::mutate(day = NA)
+    }
+    # If using verbatim_name, will want to add that as the value for the 
+    # species column (if the column does not already exist)
+    if (!("species" %in% colnames(obs))) {
+      obs <- obs %>%
+        dplyr::mutate(species = verbatim_name)
+    }
+    
+    # Select only those columns we want to write to file
+    obs <- obs %>%
+      dplyr::select(gbifID, species, longitude, latitude, 
+                    year, month, day, countryCode, basisOfRecord)
+    
+    # Before writing to file, add a column with accepted name (which
+    # may be different than what GBIF puts in species column)
+    obs$accepted_name <- species_name
+    
+    # Write to file and report back (if appropriate)
+    # write.csv(x = obs,
+    #           file = filename,
+    #           row.names = FALSE)
+    # 
+    # if (verbose) {
+    #   message(paste0(nrow(obs), " records of ", species_name, 
+    #                  " written to ", filename))
+    # }
+    return(obs)
+  } else {
+    if (verbose) {
+      message(paste0("After filtering, no records of ", species_name,
+                     " saved to disk."))
     }
   }
 }
--- src/data/gbif-1-download.R
+++ src/data/gbif-1-download.R
@@ -3,7 +3,7 @@
 # jcoliver@arizona.edu
 # 2021-06-02
 
-# require(dplyr) # currently only needed for testing subset
+require(dplyr) # For binding results when "other name" exists
 
 # Load up the functions from the functions folder
 source(file = "load_functions.R")
@@ -28,13 +28,53 @@ sink()
 # Loop over all entries in gbif data and do queries
 for (i in 1:nrow(gbif_data)) {
   species_name <- gbif_data$accepted_name[i]
-  gbif_name <- gbif_data$gbif_name[i]
-  download_gbif(species_name = species_name,
-                gbif_name = gbif_name,
-                replace = replace,
-                verbose = TRUE,
-                max_attempts = 10,
-                logfile = logfile)
+  nice_name <- tolower(x = gsub(pattern = " ",
+                                replacement = "_",
+                                x = species_name))
+  filename <- paste0("data/gbif/downloaded/", nice_name, "-gbif.csv")
+  # Only proceed if file doesn't exist or we want to replace existing files
+  if (!file.exists(filename) | replace) {
+    gbif_name <- gbif_data$gbif_name[i]
+    obs <- download_gbif(species_name = species_name,
+                         gbif_name = gbif_name,
+                         replace = replace,
+                         verbose = TRUE,
+                         max_attempts = 10,
+                         logfile = logfile)
+    # Check for other names (where the name of the thing on GBIF might not be 
+    # an accepted name, and thus not come back in a "normal" query; e.g. 
+    # records of Papilio bairdii oregonia will not be returned with normal 
+    # query, as P. bairdii is not recognized as a valid name on GBIF)
+    other_names <- unlist(strsplit(x = trimws(x = gbif_data$other_names[i]),
+                                   split = ","))
+    if (length(other_names) > 0) {
+      # Now here's a spot to get fancy with lapply, or be lazy and loop
+      # Loop it is!
+      for (other_name in other_names) {
+        other_obs <- download_gbif(species_name = species_name,
+                                   verbatim_name = other_name,
+                                   replace = replace,
+                                   verbose = TRUE,
+                                   max_attempts = 10,
+                                   logfile = logfile)
+        obs <- obs %>%
+          bind_rows(other_obs)
+      }
+    }
+
+    write.csv(x = obs,
+              file = filename,
+              row.names = FALSE)
+    
+    if (verbose) {
+      message(paste0(nrow(obs), " records of ", species_name,
+                     " written to ", filename))
+    }
+  } else {
+    if (verbose) {
+      message(species_name, " records already on disk and rerun set to FALSE.")
+    }
+  }
   # Adding a 2 second sleep to slow things down.
   Sys.sleep(time = 2)
   if (i %% print_freq == 0 | i == 1) {
