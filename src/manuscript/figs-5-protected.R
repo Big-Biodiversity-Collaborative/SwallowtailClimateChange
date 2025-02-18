688b5d02bc1cbdcd60eacc9b40028cae13d871d3
Jeff Oliver <jcoliver@arizona.edu>
2025-02-18 14:24:59 -0800

    Get protected areas sizing right

--- src/manuscript/figs-5-protected.R
+++ src/manuscript/figs-5-protected.R
@@ -141,27 +141,35 @@ hotspots_long <- hotspots_long %>%
 #   3. Stacked area percentage protected
 ssps <- unique(hotspots_long$ssp)
 
-# List of three elements (one element per SSP). Each element is list of three 
-# ggplot objects
-ssp_plots <- vector(mode = "list",
-                    length = length(ssps))
-names(ssp_plots) <- ssps
-
 # TODO: May want to make sure scales (y axis limits) are consistent across all 
-# SSPs. Not currently implemented - ylim() commented out in each plot.
+# SSPs. Not currently implemented; ylim() commented out in each plot.
 total_ylim <- c(min(hotspots_long$value[hotspots_long$stat == "total"]),
                 max(hotspots_long$value[hotspots_long$stat == "total"]))
 area_ylim <- c(0, max(hotspots_long$value[hotspots_long$stat == "area"]))
 perc_ylim <- c(0, max(hotspots_long$value[hotspots_long$stat == "prop"])) * 100
 
-# TODO: In all cases, want to move the management type legend into the plot 
-# area (with a white box) in the AMOUNT area protected and remove the legend 
-# from the PERCENT area protected
-
 # Get the colors for the different management categories
 management_cols <- get_colors(palette = "protected")
 # Blech
 names(management_cols) <- tools::toTitleCase(names(management_cols))
+
+# Font sizes need to be a little smaller
+axis_text_size <- 6
+axis_title_size <- 8
+legend_title_size <- 6
+legend_text_size <- 4
+legend_key_height <- 10
+legend_key_width <- 12
+legend_margin <- margin(c(0, 0, 0, 0))
+
+# Need to add margins to plot to avoid overlapping with panel labels
+margins <- c(12, 0, 0, 12)
+
+# List of three elements (one element per SSP). Each element is list of three 
+# ggplot objects
+ssp_plots <- vector(mode = "list",
+                    length = length(ssps))
+names(ssp_plots) <- ssps
 
 for (ssp_i in 1:length(ssps)) {
   # Pull out data for this SSP
@@ -176,9 +184,12 @@ for (ssp_i in 1:length(ssps)) {
                        mapping = aes(x = yr_midpoint, y = value/1000)) +
     geom_point() +
     geom_line() +
-    labs(x = "Year", y = "Total Area (1K sq km)") +
-    # ylim(total_ylim) +
-    theme_bw()
+    labs(x = "Year", 
+         y = expression(paste("Total Area (1K km"^2, ")"))) +
+  # ylim(total_ylim) +
+    theme_bw() +
+    theme(axis.title = element_text(size = axis_title_size),
+          axis.text = element_text(size = axis_text_size))
   plot_list[[1]] <- line_total
     
   # Stacked area amount of area protected (in sq km)
@@ -189,11 +200,20 @@ for (ssp_i in 1:length(ssps)) {
     geom_area() +
     scale_fill_manual(name = "Management",
                       values = management_cols) +
-    labs(x = "Year", y = "Protected Area (1K sq km)") +
+    labs(x = "Year", 
+         y = expression(paste("Protected Area (1K km"^2, ")"))) +
     # ylim(area_ylim) +
     theme_bw() +
     theme(legend.position = "inside",
-          legend.position.inside = c(0.8, 0.55))
+          legend.position.inside = c(0.8, 0.55),
+          legend.text = element_text(size = legend_text_size),
+          legend.title = element_text(size = legend_title_size),
+          legend.key.height = unit(legend_key_height, "pt"),
+          legend.key.width = unit(legend_key_width, "pt"),
+          # legend.margin = legend_margin,
+          # plot.margin = unit(margins, "pt"),
+          axis.title = element_text(size = axis_title_size),
+          axis.text = element_text(size = axis_text_size))
   plot_list[[2]] <- stacked_area
 
   # Stacked area percentage of area protected (100 X prop values)
@@ -208,7 +228,10 @@ for (ssp_i in 1:length(ssps)) {
     labs(x = "Year", y = "% Area Protected") +
     # ylim(perc_ylim) +
     theme_bw() +
-    theme(legend.position = "none")
+    theme(legend.position = "none",
+          # plot.margin = unit(margins, "pt"),
+          axis.title = element_text(size = axis_title_size),
+          axis.text = element_text(size = axis_text_size))
   plot_list[[3]] <- stacked_perc
   
   # Finally, add that list to the appropriate spot in the ssp list
@@ -226,12 +249,14 @@ main_plot_list <- ssp_plots[[main_ssp]]
 main_plot <- plot_grid(plotlist = main_plot_list,
                        align = "h",
                        ncol = 1,
-                       labels = "auto")
+                       labels = "auto",
+                       vjust = 1,
+                       hjust = 0)
 # main_plot
 ggsave(filename = main_plot_file,
        plot = main_plot,
        width = 3,
-       height = 5,
+       height = 8,
        units = "in")
 
 # Now create same thing, but with remaining SSPs
@@ -250,5 +275,5 @@ supp_plot <- plot_grid(plotlist = supp_plot_list,
 ggsave(filename = supp_plot_file,
        plot = supp_plot,
        width = 6,
-       height = 5,
+       height = 8,
        units = "in")
