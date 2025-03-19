e97a542c02a18aefc672b4034c87f55d045cc293
Jeff Oliver <jcoliver@email.arizona.edu>
2025-03-19 16:11:12 -0700

    Work on distributions figures, shifting to functions

--- src/manuscript/figs-4-distributions.R
+++ src/manuscript/figs-4-distributions.R
@@ -38,19 +38,24 @@ source(file = "functions/get_colors.R")
 #   + Page 2, column 1: suitable areas for 2071 for three SSPs
 #   + Page 2, column 2: differences in area from current predicted areas
 
+# DEPRECATED:
 # To ensure consistency across figures, we will go ahead and make plot objects
 # for each species (that we have the appropriate data for), and store these in 
 # a list (of lists?). We'll do subsequent iteration across the list to create 
 # the figure for the main manuscript and for the supplemental. Note 
 # supplemental will be slightly duplicative, as it will also include the two 
 # species in the main manuscript figure.
+# UPDATE: Nope. Too RAM intensive.
 
+# DEPRECATED:
 # If replace_ggplots is TRUE, the script will re-create individual plots, save 
 # them to local files (in output/manuscript/distribution-figs), and use those 
 # figures to create manuscript figures. If FALSE, it will attempt to read files
 # from disk and use those plots for manuscript figures (if file does not exist 
 # on disk and replace_ggplots is FALSE, script will attempt to make the ggplot 
 # object and save it)
+# UPDATE: This doesn't really work, as the ggplot objects include references to 
+# variables that may or may not exist.
 replace_ggplots <- FALSE
 
 ih <- read.csv("data/insect-host.csv")
@@ -72,6 +77,9 @@ nice_names <- tolower(gsub(pattern = " ",
 states <- vect("data/political-boundaries/states.shp")  
 countries <- vect("data/political-boundaries/countries.shp")
 
+# Directory where figures are heading
+output_basename <- "output/manuscript/"
+
 # Reproject states and countries (can take a couple moments with first call to 
 # project)
 states <- project(states, "ESRI:102009")
@@ -83,6 +91,7 @@ countries <- project(countries, crs(states))
 countries <- countries %>%
   filter(adm0_a3 %in% c("USA", "CAN", "MEX"))
 
+# TODO: Are these two plots lists necessary?
 # List to hold current species predictions
 current_plots <- vector(mode = "list", length = length(nice_names))
 names(current_plots) <- nice_names
@@ -95,44 +104,14 @@ names(species_plots) <- nice_names
 scenarios <- c("ssp245", "ssp370", "ssp585")
 times <- c("2041", "2071")
 
-# TODO: testing iteration stuff here. Skipping for now. Requires combinatorics 
-# expertise I don't have the time for.
-# species <- c("indra", "rumiko", "rutulus", "zelicaon")
-# for (species_i in 1:length(species)) {
-#   one_species <- species[species_i]
-#   for (time_i in 1:length(times)) {
-#     time <- times[time_i]
-#     for (scenario_i in 1:length(scenarios)) {
-#       scenario <- scenarios[scenario_i]
-#       # For the list of plots for this species (up to 12 plots), we need to 
-#       # keep track of the forecast plot and the delta plot for this 
-#       # scenario/time combination.
-#       #  1  2   SSP2 (1)  2041 (1)
-#       #  3  4   SSP3 (2)  2041 (1)
-#       #  5  6   SSP5 (3)  2041 (1)
-#       #  7  8   SSP2 (1)  2071 (2)
-#       #  9 10   SSP3 (2)  2071 (2)
-#       # 11 12   SSP5 (3)  2071 (2)
-#       
-#       # Starting point is how many full sets have already been done
-#       base <- (time_i - 1) * length(scenarios) * 2 + 1 
-#       scenario_add <- (scenario_i - 1) * 2
-#       forecast_i <- (time_i - 1) * length(scenarios) * 2 + 1 + (scenario_i - 1) * 2
-#       delta_i <- forecast_i + 1
-#       
-#       # "model" is the text combination of the scenario (ssp) & time (year)
-#       model <- paste0(scenario, "_", time)
-#       
-#       message(model, ": ", forecast_i, ", ", delta_i)
-#     }
-#   }
-# }      
-
 # Get colors for plots
 dist_cols <- get_colors(palette = "overlap")
 # TODO: Consider moving this to the get_colors() function
 names(dist_cols) <- c("Absent", "Hosts only", "Swallowtail only", 
                       "Swallowtail and hosts")  
+# names(dist_cols) <- tools::toTitleCase(gsub(pattern = "_",
+#                                             replacement = " ", 
+#                                             x = names(dist_cols)))
 
 delta_cols <- get_colors(palette = "distdelta")
 names(delta_cols) <- tools::toTitleCase(names(delta_cols))  
@@ -142,25 +121,27 @@ state_fill <- "white"
 state_color <- "gray50"
 countries_color <- "black"
 
-# Main manuscript figure for two species
-main_species <- c("papilio_cresphontes", "papilio_rumiko")
+# A list to pass to plotting functions. Are we going overboard on the Law of 
+# Demeter? Maybe. Maybe we are.
+plot_params <- list(states = states,
+                    countries = countries,
+                    dist_cols = dist_cols,
+                    delta_cols = delta_cols,
+                    state_fill = state_fill, 
+                    state_color = state_color,
+                    countries_color = countries_color)
+################################################################################
+# Define the functions that will make the plots
+# Current plot
 
-# + Predicted suitable areas for contemporary climate
-# + Predicted suitable areas for 2050s, under SSP3-7.0
-# + Difference in area between two predictions
-
-main_plots_list <- vector(mode = "list", length = 6)
-
-# Iterate over the species of interest
-for (i in 1:length(main_species)) {
-  one_species <- main_species[i]
-  # Indices for list elements so things get put in the right place
-  current_index <- (i - 1) * 3 + 1
-  forecast_index <- current_index + 1
-  delta_index <- current_index + 2
-  
-  # Get raster of contemporary predictions
-  current <- readRDS(paste0("output/overlaps/", one_species,
+#' Make plot of current suitable area
+#' 
+#' @param nice_name character Compute-friendly name, i.e. "papilio_rumiko"
+#' @param plot_params list Plot parameters, such as colors and state/country 
+#' border objects
+#' @return ggplot object
+current_ggplot <- function(nice_name, plot_params) {
+  current <- readRDS(paste0("output/overlaps/", nice_name,
                             "-overlap-current.rds"))
   # Update the rasters to have values we want (1, 2, 3, 4). Rasters coming in 
   # should have the following values:
@@ -176,10 +157,10 @@ for (i in 1:length(main_species)) {
   current[current == 3] <- 2
   current[current %in% 4:5] <- 3
   current <- as.factor(current)
-  levels(current) <- data.frame(value = 0:3, label = names(dist_cols))
+  levels(current) <- data.frame(value = 0:3, label = names(plot_params$dist_cols))
   
   # Do re-projection to Lambert & drop missing
-  current <- project(current, crs(states), method = "near")
+  current <- project(current, crs(plot_params$states), method = "near")
   current <- drop_na(current)
   
   # Figure out extent for plot (otherwise extent is based on CAN, MEX, USA)
@@ -187,101 +168,182 @@ for (i in 1:length(main_species)) {
   ylim_current <- c(ext(current)[3], ext(current)[4])
   
   # Make plot for contemporary predictions
-  main_plots_list[[current_index]] <- ggplot() +
-    geom_spatvector(data = states, color = NA, fill = state_fill) +
+  current_plot <- ggplot() +
+    geom_spatvector(data = plot_params$states, color = NA, fill = plot_params$state_fill) +
     geom_spatraster(data = current, maxcell = Inf) +
-    scale_fill_manual(name = "label", values = dist_cols, na.translate = FALSE) +
-    geom_spatvector(data = states, color = state_color, fill = NA) +
-    geom_spatvector(data = countries, color = countries_color, fill = NA) +
+    scale_fill_manual(name = "label", values = plot_params$dist_cols, na.translate = FALSE) +
+    geom_spatvector(data = plot_params$states, color = plot_params$state_color, fill = NA) +
+    geom_spatvector(data = plot_params$countries, color = plot_params$countries_color, fill = NA) +
     coord_sf(datum = sf::st_crs("EPSG:4326"), 
              xlim = xlim_current, ylim = ylim_current) +
     theme_bw() +
     theme(legend.position = "none")
   ggplot2::set_last_plot(NULL)
   invisible(gc())
-  
-  # Make forecast plot for SSP3-7.0, 2050s
-  model <- "ssp370_2041"
-  
-  forecast_plot_file <- paste0("output/manuscript/distribution-figs/",
-                               one_species, "-ggplot-", model, ".rds")
-    # Get forecast raster
-    forecast <- readRDS(paste0("output/overlaps/", one_species,
-                               "-overlap-ensemble_", model, ".rds"))
-    
-    # Update to 0-3 scale
-    forecast[forecast %in% 1:2] <- 1
-    forecast[forecast == 3] <- 2
-    forecast[forecast %in% 4:5] <- 3
-    forecast <- as.factor(forecast)
-    levels(forecast) <- data.frame(value = 0:3, label = names(dist_cols))
-    
-    # Do re-projection to Lambert & drop missing
-    forecast <- project(forecast, crs(states), method = "near")
-    forecast <- drop_na(forecast)
-    
-    # Figure out extent for plot (otherwise extent is based on CAN, MEX, USA)
-    xlim_forecast <- c(ext(forecast)[1], ext(forecast)[2])
-    ylim_forecast <- c(ext(forecast)[3], ext(forecast)[4])
-    
-    # Make plot for forecast predictions
-    main_plots_list[[forecast_index]] <- ggplot() +
-      geom_spatvector(data = states, color = NA, fill = state_fill) +
-      geom_spatraster(data = forecast, maxcell = Inf) +
-      scale_fill_manual(name = "label", values = dist_cols, na.translate = FALSE) +
-      geom_spatvector(data = states, color = state_color, fill = NA) +
-      geom_spatvector(data = countries, color = countries_color, fill = NA) +
-      coord_sf(datum = sf::st_crs("EPSG:4326"), 
-               xlim = xlim_forecast, 
-               ylim = ylim_forecast) +
-      theme_bw() +
-      theme(legend.position = "none")
-    ggplot2::set_last_plot(NULL)
-  invisible(gc())
+  return(current_plot)
+}
 
-  delta_plot_file <- paste0("output/manuscript/distribution-figs/",
-                            one_species, "-ggplot-delta-", model, ".rds")
-    # Get delta raster
-    delta <- readRDS(paste0("output/deltas/", one_species,
-                            "-delta-insecthost-", model, ".rds"))
-    
-    # See src/summary/summary-2-compare-ranges.R
-    delta <- as.factor(delta)
-    levels(delta) <- data.frame(value = 0:3, label = names(delta_cols))
-    
-    # Do re-projection to Lambert & drop missing
-    delta <- project(delta, crs(states), method = "near")
-    delta <- drop_na(delta)
-    
-    # Figure out extent for plot (otherwise extent is based on CAN, MEX, USA)
-    xlim_delta <- c(ext(delta)[1], ext(delta)[2])
-    ylim_delta <- c(ext(delta)[3], ext(delta)[4])
-    
-    # Make plot for change in predicted ranges
-    main_plots_list[[delta_index]] <- ggplot() +
-      geom_spatvector(data = states, color = NA, fill = state_fill) +
-      geom_spatraster(data = delta, maxcell = Inf) +
-      scale_fill_manual(name = "label", values = delta_cols, na.translate = FALSE) +
-      geom_spatvector(data = states, color = state_color, fill = NA) +
-      geom_spatvector(data = countries, color = countries_color, fill = NA) +
-      coord_sf(datum = sf::st_crs("EPSG:4326"), 
-               xlim = xlim_delta, 
-               ylim = ylim_delta) +
-      theme_bw() +
-      theme(legend.position = "none")
-    ggplot2::set_last_plot(NULL)
-    invisible(gc())
+#' Make plot of forecast suitable area
+#' 
+#' @param nice_name character Compute-friendly name, i.e. "papilio_rumiko"
+#' @param model character Descriptor of climate model and time period, e.g. 
+#' "ssp370_2041"
+#' @param plot_params list Plot parameters, such as colors and state/country 
+#' border objects
+#' @return ggplot object
+forecast_ggplot <- function(nice_name, model, plot_params) {
+  # Get forecast raster
+  forecast <- readRDS(paste0("output/overlaps/", nice_name,
+                             "-overlap-ensemble_", model, ".rds"))
+  
+  # Update to 0-3 scale
+  forecast[forecast %in% 1:2] <- 1
+  forecast[forecast == 3] <- 2
+  forecast[forecast %in% 4:5] <- 3
+  forecast <- as.factor(forecast)
+  levels(forecast) <- data.frame(value = 0:3, label = names(plot_params$dist_cols))
+  
+  # Do re-projection to Lambert & drop missing
+  forecast <- project(forecast, crs(plot_params$states), method = "near")
+  forecast <- drop_na(forecast)
+  
+  # Figure out extent for plot (otherwise extent is based on CAN, MEX, USA)
+  xlim_forecast <- c(ext(forecast)[1], ext(forecast)[2])
+  ylim_forecast <- c(ext(forecast)[3], ext(forecast)[4])
+  
+  # Make plot for forecast predictions
+  forecast_plot <- ggplot() +
+    geom_spatvector(data = plot_params$states, color = NA, fill = plot_params$state_fill) +
+    geom_spatraster(data = forecast, maxcell = Inf) +
+    scale_fill_manual(name = "label", values = plot_params$dist_cols, na.translate = FALSE) +
+    geom_spatvector(data = plot_params$states, color = plot_params$state_color, fill = NA) +
+    geom_spatvector(data = plot_params$countries, color = plot_params$countries_color, fill = NA) +
+    coord_sf(datum = sf::st_crs("EPSG:4326"), 
+             xlim = xlim_forecast, 
+             ylim = ylim_forecast) +
+    theme_bw() +
+    theme(legend.position = "none")
+  ggplot2::set_last_plot(NULL)
+  invisible(gc())
+  return(forecast_plot)
+}
+
+#' Make plot of difference between current and forecast suitable area
+#' 
+#' @param nice_name character Compute-friendly name, i.e. "papilio_rumiko"
+#' @param model character Descriptor of climate model and time period, e.g. 
+#' "ssp370_2041"
+#' @param plot_params list Plot parameters, such as colors and state/country 
+#' border objects
+#' @return ggplot object
+delta_ggplot <- function(nice_name, model, plot_params) {
+  # Get delta raster
+  delta <- readRDS(paste0("output/deltas/", nice_name,
+                          "-delta-insecthost-", model, ".rds"))
+  
+  # See src/summary/summary-2-compare-ranges.R
+  delta <- as.factor(delta)
+  levels(delta) <- data.frame(value = 0:3, label = names(plot_params$delta_cols))
+  
+  # Do re-projection to Lambert & drop missing
+  delta <- project(delta, crs(plot_params$states), method = "near")
+  delta <- drop_na(delta)
+  
+  # Figure out extent for plot (otherwise extent is based on CAN, MEX, USA)
+  xlim_delta <- c(ext(delta)[1], ext(delta)[2])
+  ylim_delta <- c(ext(delta)[3], ext(delta)[4])
+  
+  # Make plot for change in predicted ranges
+  delta_plot <- ggplot() +
+    geom_spatvector(data = plot_params$states, color = NA, fill = plot_params$state_fill) +
+    geom_spatraster(data = delta, maxcell = Inf) +
+    scale_fill_manual(name = "label", values = plot_params$delta_cols, na.translate = FALSE) +
+    geom_spatvector(data = plot_params$states, color = plot_params$state_color, fill = NA) +
+    geom_spatvector(data = plot_params$countries, color = plot_params$countries_color, fill = NA) +
+    coord_sf(datum = sf::st_crs("EPSG:4326"), 
+             xlim = xlim_delta, 
+             ylim = ylim_delta) +
+    theme_bw() +
+    theme(legend.position = "none")
+  ggplot2::set_last_plot(NULL)
+  invisible(gc())
+  return(delta_plot)
+}
+
+################################################################################
+# Figure [1] (not Figure 1 in manuscript)
+# Main manuscript figure for two species
+main_species <- c("papilio_cresphontes", "papilio_rumiko")
+
+# + Predicted suitable areas for contemporary climate
+# + Predicted suitable areas for 2050s, under SSP3-7.0
+# + Difference in area between two predictions
+
+main_plots_list <- vector(mode = "list", length = 6)
+
+model <- "ssp370_2041"
+
+# Iterate over the species of interest
+for (i in 1:length(main_species)) {
+  one_species <- main_species[i]
+  # Indices for list elements so things get put in the right place
+  current_index <- (i - 1) * 3 + 1
+  forecast_index <- current_index + 1
+  delta_index <- current_index + 2
+
+  message("Plotting ", gsub(pattern = "papilio_",
+                            replacement = "Papilio ",
+                            x = one_species))
+  main_plots_list[[current_index]] <- current_ggplot(nice_name = one_species,
+                                                        plot_params = plot_params)
+  
+  main_plots_list[[forecast_index]] <- forecast_ggplot(nice_name = one_species,
+                                                          model = model,
+                                                          plot_params = plot_params)
+  
+  main_plots_list[[delta_index]] <- delta_ggplot(nice_name = one_species,
+                                                    model = model,
+                                                    plot_params = plot_params)
 }
 
 # Use the list to create the multi-panel figure
-main_plots <- cowplot::plot_grid(main_plots_list,
-                                 byrow = FALSE)
+main_plots <- cowplot::plot_grid(plotlist = main_plots_list,
+                                 byrow = FALSE,
+                                 ncol = 2,
+                                 labels = "auto", 
+                                 vjust = 1,
+                                 hjust = 0)
 
+main_dist_file <- paste0(output_basename, "Figure-Distributions.png")
+ggsave(filename = main_dist_file,
+       plot = main_plots,
+       width = 6,
+       height = 8,
+       units = "in")
 
+rm(main_plots, main_plots_list)
+ggplot2::set_last_plot(NULL)
+invisible(gc())
 
+################################################################################
+# Figure [2] (not Figure 2 in manuscript)
+# Supplemental manuscript figures for all species. Will write [XX] image files 
+# to disk and paste together later.
 
+# could be done multiple steps: first, create the pages (3) for the current 
+# distributions, then go species-by-species to create the pages (2/species) for 
+# forecast distributions.
 
+# iterate over all species
 
+# Draw the current plot. If this is sixth plot, (list length == 6) or last 
+#   species (i == length(nice_names))
+#     + make the six*-panel figure with plot_grid (*Last page won't necessarily
+#       have six panels)
+#     + save the six-panel figure to file (output/manuscript/supp-dist/ ?), 
+#       including page number
+#     + clean out the list (one_list <- NULL, grow via one_list$a <- "a")
+#     + increment the page number
 
 
 
