f5291107e93f357c7c01fd4640781979f2560c19
Jeff Oliver <jcoliver@email.arizona.edu>
2025-05-08 12:18:36 -0700

    Update slurm scripts to R 4.4 and add install terra scripts

---
+++ src/hpc/prep-install-terra.R
@@ -0,0 +1,6 @@
+# Install the terra package (for use with prep-install-terra.slurm)
+# Jeff Oliver
+# jcoliver@arizona.edu
+# 2025-05-08
+
+install.packages("terra", repos = "https://ftp.osuosl.org/pub/cran/")
