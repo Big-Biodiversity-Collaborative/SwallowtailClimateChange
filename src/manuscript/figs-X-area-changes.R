210eb70be0bc9c309b0c722139b6cb0d247eff3f
Jeff Oliver <jcoliver@email.arizona.edu>
2025-07-21 14:22:40 -0700

    Add figure of changes in area for individual species

--- src/manuscript/figs-X-area-changes.R
+++ src/manuscript/figs-X-area-changes.R
@@ -6,17 +6,25 @@
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+source(file = "functions/get_colors.R")
 
 # Plots based on the output of src/summary/summary-2-compare-ranges.R, which is 
 # written to output/summary-stats/overlap-summary-allspp.csv
 
-# area_gained: area (sqkm) predicted suitable in future that wasn't in current range
-# area_lost: area (sqkm) predicted unsuitable in future that was in current range
-# area_retained: area (sqkm) predicted suitable in future that was in current range
+# area_gained: area (sqkm) predicted suitable in future that wasn't in current 
+#   range
+# area_lost: area (sqkm) predicted unsuitable in future that was in current 
+#   range
+# area_retained: area (sqkm) predicted suitable in future that was in current 
+#   range
 # Note: Can calculate % of current range that's still suitable in future as 
 # area_retained/(area retained + area lost)
 
 range_info <- read.csv(file = "output/summary-stats/overlap-summary-allspp.csv")
+model_info <- read.csv(file = "data/climate-models.csv") %>%
+  mutate(name = gsub(pattern = "ensemble_",
+                     replacement = "",
+                     x = name))
 
 # Restrict to data where insect suitability includes overlap with area suitable 
 # for at least one host plant
@@ -24,25 +32,10 @@ range_info <- range_info %>%
   filter(distribution == "insect + host") %>%
   select(-distribution)
 
-# Need to drop "current" climate rows and separate climate into year 
-# and ssp columns
+# Add model info, mostly for text strings to use for plots
 range_info <- range_info %>%
   filter(climate != "current") %>%
-  mutate(ssp = substr(climate, start = 4, stop = 6),
-         year = substr(climate, start = 8, stop = 11)) %>%
-  # Create year information for facet labels
-  #     2041 -> 2050s
-  #     2071 -> 2080s
-  mutate(year_text = if_else(year == 2041, 
-                             true = "2050s", 
-                             false = "2080s")) %>%
-  # Create cleaner ssp labels
-  mutate(ssp_text = paste0("SSP",
-                           substr(ssp, start = 1, stop = 1),
-                           "-",
-                           substr(ssp, start = 2, stop = 2),
-                           ".",
-                           substr(ssp, start = 3, stop = 3)))
+  left_join(model_info, by = c("climate" = "name"))
 
 # Add the percent of current range retained to the data frame
 range_info <- range_info %>%
@@ -58,7 +51,8 @@ range_info <- range_info %>%
 # Now add in east/west information for coloring
 ew <- read.csv(file = "data/insect-eastwest.csv")
 range_info <- range_info %>%
-  left_join(ew)
+  left_join(ew, by = join_by(insect)) %>%
+  mutate(ew = tolower(ew)) # a hack for dealing with color names
 
 # From here on out, the species names should have the genus abbreviated
 range_info <- range_info %>%
@@ -66,17 +60,23 @@ range_info <- range_info %>%
                        replacement = "P.", 
                        x = insect))
 
-ew_colors <- c("#3eafa3", "#ce932a")
+# What is change in *current* suitable area (and only current, so we ignore any 
+# gains of suitable area)
+range_info <- range_info %>%
+  mutate(perc_current = (area_retained/(area_lost + area_retained)) * 100)
+
+ew_colors <- get_colors(palette = "eastwest")
 
 ################################################################################
 # Area changes plots
 area_long <- range_info %>%
-  filter(ssp == "370", year == 2055) %>%
-  select(insect, area_gained, area_lost, area_retained, area_delta, perc_delta, ew) %>%
+  filter(climate == "ssp370_2041") %>%
+  select(insect, area_gained, area_lost, area_retained, area_delta, 
+         perc_delta, perc_current, ew) %>%
   pivot_longer(-c(insect, ew),
                names_to = "change",
                values_to = "area_km") %>%
-  filter(!(insect %in% c("P. appalachiensis", "P. palamedes"))) %>%
+  # filter(!(insect %in% c("P. appalachiensis", "P. palamedes"))) %>%
   arrange(ew, insect) %>%
   mutate(insect = factor(x = insect,
                          levels = rev(unique(insect))))
@@ -104,25 +104,49 @@ changes_points <- ggplot(data = area_long %>%
   coord_flip()
 changes_points
 
+# TODO: This is the plot to use. May still combine by just using area, and 
+# labelling points with the percentage change
+# bquote("Area gained"~(km^2))
+facet_names <- c("area_delta" = "Change in total suitable area (1K km^2)",
+                 "perc_delta" = "Change in total suitable area (%)",
+                 "perc_current" = "Currently suitable area retained (%)")
+
 # We have delta_area, could just do plot of that. The faceted plot works OK, 
 # it might be better to label the points with the perc_delta value
+# Note just about everything (points, text) looks too small on screen but is 
+# sized for output file
 delta_points <- ggplot(data = area_long %>%
-                         filter(change %in% c("area_delta", "perc_delta")),
+                         filter(change %in% c("area_delta", "perc_delta", "perc_current")) %>%
+                         mutate(hline = if_else(change == "perc_current",
+                                                true = 100,
+                                                false = 0)) %>%
+                         mutate(area_km = if_else(change == "area_delta",
+                                                  area_km/1000,
+                                                  area_km)),
                        mapping = aes(x = insect, 
                                      y = area_km,
                                      color = ew)) +
-  geom_hline(yintercept = 0, linetype = 2, linewidth = 0.5) +
-  geom_point(size = 4) +
+  # geom_hline(yintercept = 0, linetype = 2, linewidth = 0.5) +
+  geom_hline(mapping = aes(yintercept = hline), linetype = 2, linewidth = 0.5) +
+  geom_point(size = 2) +
   scale_color_manual(name = "East/West",
                      values = ew_colors) +
   coord_flip() +
   theme_bw() +
-  facet_grid(~ change, scale = "free_x") +
-  ylab(label = bquote("Change in Area"~(km^2))) +
-  theme(axis.text.y = element_text(face = "italic", size = 10),
+  facet_grid(~ change, scale = "free_x", labeller = as_labeller(facet_names)) +
+  # expand_limits(y = -100) +
+  theme(axis.text.y = element_text(face = "italic", size = 9),
+        axis.text.x = element_text(size = 6),
         axis.title.y = element_blank(),
-        axis.title.x = element_blank())
+        axis.title.x = element_blank(),
+        strip.background = element_blank(),
+        strip.text = element_text(size = 6),
+        legend.position = "none")
 delta_points
+ggsave(file = "output/manuscript/Figure-Area-Change.png",
+       plot = delta_points,
+       width = 6,
+       height = 4)
 
 # Area gained 
 gained <- ggplot(data = range_info, mapping = aes(x = area_gained,
@@ -133,7 +157,7 @@ gained <- ggplot(data = range_info, mapping = aes(x = area_gained,
                     values = ew_colors) +
   xlab(label = bquote("Area gained"~(km^2))) +
   ylab(label = expression(paste("# ", italic("Papilio"), " species"))) +
-  facet_grid(ssp ~ year) +
+  facet_grid(ssp ~ yr_text) +
   theme_bw()
 gained
 
