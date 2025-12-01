244ae94255b95cb07c495b324dd0303163a85492
Jeff Oliver <jcoliver@email.arizona.edu>
2025-12-01 13:18:19 -0700

    Add scaffolds for protected area richness analyses

---
+++ src/summary/protected-areas-5-species.R
@@ -0,0 +1,42 @@
+# Look at individual species' suitability in and out of protected areas
+# Jeff Oliver
+# jcoliver@arizona.edu
+# 2025-12-01
+
+# For each species, want to know 
+#    + how much currently suitable area is in protected
+#    + how much forecast suitable area is in protected
+
+# Across all species, is the amount current suitable & protected significantly 
+# different than forecast suitable & protected
+
+# Read in species list
+
+# Read in climate model info
+
+# Read in protected habitat info
+
+# Create data frame to hold information on amount protected/not protected
+# Species | climate | amount suitable & protected | amount suitable & not protected
+
+# Create list (?) to hold statistical results (one element per forecast model?)
+
+# Iterate over species
+
+# Read in current suitable
+
+# Calculate amount suitable & protected/unprotected
+
+# Iterate over forecast climate
+
+# Read in forecast suitable
+
+# Calculate amount suitable & protected/unprotected
+
+# Run comparison to current (t-test?)
+
+# End iterating over forecast climate
+
+# End iterating over species
+
+# Do some sort of visualization?
---
+++ src/summary/protected-areas-6-richness.R
@@ -0,0 +1,33 @@
+# Look at richness in protected and unprotected areas
+# Jeff Oliver
+# jcoliver@arizona.edu
+# 2025-12-01
+
+# Want to know average richness in protected vs. unprotected areas (maybe not?)
+#    TODO: For the above, may need to decide which areas to look at - all of 
+#          North America?
+# Want to compare richness in protected areas in current vs. future climates
+
+# Read in protected areas info
+
+# Read in current richness raster
+
+# Create data frame to hold info for each climate model
+
+# Create list (?) to hold statistical analysis results
+
+# Calculate average richness in protected areas
+
+# Iterate over forecast climate models
+
+# Read in forecast richness raster
+
+# Calculate average richness in protected areas
+
+# TODO: Is there a way to do cell-by-cell comparison, as protected areas are 
+# not changing between current and forecast? Seems like this would be 
+# especially appropriate for a paired t-test
+
+# Do t-test (or other?) to compare current vs forecast richness
+
+# End iterating over all forecast climate models
