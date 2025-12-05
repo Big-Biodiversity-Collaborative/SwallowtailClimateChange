23a6291626dc6e3d3a35e9c0b0f88e4aac5f8893
Jeff Oliver <jcoliver@email.arizona.edu>
2025-12-05 12:16:16 -0700

    Add summary stat richness calculations on protected areas

--- src/summary/protected-areas-6-richness.R
+++ src/summary/protected-areas-6-richness.R
@@ -3,31 +3,101 @@
 # jcoliver@arizona.edu
 # 2025-12-01
 
+require(dplyr)
+require(terra)
+
 # Want to know average richness in protected vs. unprotected areas (maybe not?)
 #    TODO: For the above, may need to decide which areas to look at - all of 
 #          North America?
 # Want to compare richness in protected areas in current vs. future climates
 
-# Read in protected areas info
+# Read in protected areas info (may take 3 minutes or so)
+shpfile_path <- "data/protected-areas/protected-areas-categorized.shp"
+message("Reading protected areas shapefile (may take a few minutes)...")
+protected_areas <- terra::vect(shpfile_path)
+message("Protected areas shapefile read.")
+
+# Load list of climate models
+climate_models <- read.csv(file = "data/climate-models.csv")
+climate_models <- climate_models %>%
+  mutate(name_short = gsub(pattern = "ensemble_",
+                           replacement = "",
+                           x = name))
+  
+# Create list to hold summary stats for each model
+protected_stats <- vector(mode = "list", length = nrow(climate_models))
+names(protected_stats) <- climate_models$name_short
+
+# Create list to hold statistical analysis results
+richness_t <- vector(mode = "list", length = nrow(climate_models) - 1)
+names(richness_t) <- climate_models$name_short[-1]
 
 # Read in current richness raster
+current_richness <- terra::rast(x = "output/richness/current-richness-ov.rds")
 
-# Create data frame to hold info for each climate model
+# Calculate average richness in protected areas for current climate
+# Pull out richness values for each type of protected area (takes less than a 
+# minute). We need individual cell richness values for subsequent t-tests
+cell_richness <- exactextractr::exact_extract(x = current_richness, 
+                                      y = sf::st_as_sf(protected_areas),
+                                      progress = FALSE)
+# Resultant list is unnamed, so we want to name it
+names(cell_richness) <- data.frame(protected_areas)$AGNCY_SHOR
 
-# Create list (?) to hold statistical analysis results
-
-# Calculate average richness in protected areas
+# We can use exact_extract to get summary stats, too, with means and standard 
+# deviation weighted by cell coverage
+summary_stats <- exactextractr::exact_extract(x = current_richness,
+                                              y = sf::st_as_sf(protected_areas),
+                                              progress = FALSE,
+                                              fun = c("mean", "stdev"),
+                                              force_df = TRUE)
+summary_stats <- cbind(agency = data.frame(protected_areas)$AGNCY_SHOR, 
+                       summary_stats)
+protected_stats[["current"]] <- summary_stats
 
 # Iterate over forecast climate models
+forecast_models <- climate_models %>%
+  filter(name != "current")
 
-# Read in forecast richness raster
+for (model_i in 1:nrow(forecast_models)) {
+  model_name <- forecast_models$name[model_i]
+  model_name_short <- forecast_models$name_short[model_i]
 
-# Calculate average richness in protected areas
+  # Read in forecast richness raster
+  forecast_file <- paste0("output/richness/",
+                          model_name, 
+                          "-richness-ov.rds")
+  forecast_richness <- terra::rast(forecast_file)
+    
+  # Calculate average richness in protected areas
+  message("Calculating richness and summary stats for ", model_name_short)
+  cell_richness <- exactextractr::exact_extract(x = forecast_richness, 
+                                                y = sf::st_as_sf(protected_areas),
+                                                progress = FALSE)
+  # Resultant list is unnamed, so we want to name it
+  names(cell_richness) <- data.frame(protected_areas)$AGNCY_SHOR
+  
+  # Get summary stats for each protected area type
+  summary_stats <- exactextractr::exact_extract(x = forecast_richness,
+                                                y = sf::st_as_sf(protected_areas),
+                                                progress = FALSE,
+                                                fun = c("mean", "stdev"),
+                                                force_df = TRUE)
+  summary_stats <- cbind(agency = data.frame(protected_areas)$AGNCY_SHOR, 
+                         summary_stats)
+  protected_stats[[model_name_short]] <- summary_stats
+  
+  # TODO: Do t-test (or other?) to compares current vs forecast richness
+  
+} # End iterating over all forecast climate models
 
-# TODO: Is there a way to do cell-by-cell comparison, as protected areas are 
-# not changing between current and forecast? Seems like this would be 
-# especially appropriate for a paired t-test
+# Combine summary stats list elements into a single data frame
+protected_stats <- protected_stats %>%
+  bind_rows(.id = "climate_model")
 
-# Do t-test (or other?) to compare current vs forecast richness
+# Write those summary stats to file
 
-# End iterating over all forecast climate models
+# Extract t-test results
+
+# May need to kinda do this piecemeal. Will need some form of multiple 
+# comparison correction. Holm-Bonferroni makes sense.
